d85ae1f5b96367059a24a6f5c8e5913e5e4d4d23
Sebastian <mail2stehle@gmail.com>
2016-12-14 21:50:10 +0100

    Cleanup

---
+++ tests/Squidex.Infrastructure.Tests/TypeNameAttributeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Squidex.Infrastructure
+{
+    public class TypeNameAttributeTest
+    {
+        [Fact]
+        public void Should_instantiate()
+        {
+            var attribute = new TypeNameAttribute("MyTypeName");
+
+            Assert.Equal("MyTypeName", attribute.TypeName);
+        }
+    }
+}
