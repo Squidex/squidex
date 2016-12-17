627dc8ef89c13fa091d0e943a2e19190896637b5
Sebastian <mail2stehle@gmail.com>
2016-12-17 14:47:53 +0100

    Vars cleanup

--- src/Squidex/app/features/schemas/pages/schemas-page.component.ts
+++ src/Squidex/app/features/schemas/pages/schemas-page.component.ts
@@ -10,6 +10,8 @@ import { Component } from '@angular/core';
 import {
     AppComponentBase,
     AppsStoreService,
+    fadeAnimation,
+    ModalView,
     NotificationService,
     UsersProviderService
  } from 'shared';
@@ -17,11 +19,20 @@ import {
 @Component({
     selector: 'sqx-schemas-page',
     styleUrls: ['./schemas-page.component.scss'],
-    templateUrl: './schemas-page.component.html'
+    templateUrl: './schemas-page.component.html',
+    animations: [
+        fadeAnimation
+    ]
 })
 export class SchemasPageComponent extends AppComponentBase {
+    public modalDialog = new ModalView();
+
     constructor(apps: AppsStoreService, notifications: NotificationService, users: UsersProviderService) {
         super(apps, notifications, users);
+    }
+
+    public createSchema() {
+        this.modalDialog.show();
     }
 }
 
