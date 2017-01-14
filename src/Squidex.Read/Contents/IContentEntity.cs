0edfef19c32154c2cdf5cab484aa876e16da3b07
Sebastian <mail2stehle@gmail.com>
2017-01-14 12:31:06 +0100

    Content Repository

---
+++ src/Squidex.Read/Contents/IContentEntity.cs
@@ -0,0 +1,17 @@
+﻿// ==========================================================================
+//  IContentEntity.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using Newtonsoft.Json.Linq;
+
+namespace Squidex.Read.Contents
+{
+    public interface IContentEntity : IEntity
+    {
+        JToken Data { get; }
+    }
+}
---
+++ src/Squidex.Read/Contents/Repositories/IContentRepoitory.cs
@@ -0,0 +1,21 @@
+﻿// ==========================================================================
+//  IContentRepoitory.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Squidex.Read.Contents.Repositories
+{
+    public interface IContentRepoitory
+    {
+        Task<List<IContentEntity>> QueryAsync();
+
+        Task<IContentEntity> FindContentAsync(Guid id);
+    }
+}
