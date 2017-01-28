acfa88b18757dc76796217c406a43c81e79f565e
Sebastian <mail2stehle@gmail.com>
2017-01-28 21:44:38 +0100

    Docker improvements

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!bin/bash
 set -e
+cd src/Squidex
+npm install
+npm rebuild node-sass
+npm rebuild phantomjs-prebuilt
+npm run test:coverage
+npm run build:copy
+npm run build
+cd ../..
 dotnet restore
 dotnet test tests/Squidex.Core.Tests/project.json
 dotnet test tests/Squidex.Infrastructure.Tests/project.json
