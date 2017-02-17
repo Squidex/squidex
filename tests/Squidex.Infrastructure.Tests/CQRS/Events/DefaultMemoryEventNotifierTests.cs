bff54251c191eaecbda2ed8009568c4e9241ce54
Sebastian <mail2stehle@gmail.com>
2017-02-17 20:22:33 +0100

    Some more tests

---
+++ tests/Squidex.Infrastructure.Tests/CQRS/Events/DefaultMemoryEventNotifierTests.cs
@@ -0,0 +1,41 @@
+﻿// ==========================================================================
+//  DefaultMemoryEventNotifierTests.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using Xunit;
+
+namespace Squidex.Infrastructure.CQRS.Events
+{
+    public sealed class DefaultMemoryEventNotifierTests
+    {
+        private readonly DefaultMemoryEventNotifier sut = new DefaultMemoryEventNotifier(new InMemoryPubSub());
+
+        [Fact]
+        public void Should_invalidate_all_actions()
+        {
+            var handler1Handled = 0;
+            var handler2Handled = 0;
+
+            sut.Subscribe(() =>
+            {
+                handler1Handled++;
+            });
+
+            sut.NotifyEventsStored();
+
+            sut.Subscribe(() =>
+            {
+                handler2Handled++;
+            });
+
+            sut.NotifyEventsStored();
+
+            Assert.Equal(2, handler1Handled);
+            Assert.Equal(1, handler2Handled);
+        }
+    }
+}
---
+++ tests/Squidex.Infrastructure.Tests/InMemoryPubSubTests.cs
@@ -0,0 +1,51 @@
+﻿// ==========================================================================
+//  InMemoryPubSubTests.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace Squidex.Infrastructure
+{
+    public class InMemoryPubSubTests
+    {
+        private readonly InMemoryPubSub sut = new InMemoryPubSub();
+
+        [Fact]
+        public void Should_publish_to_handlers()
+        {
+            var channel1Events = new List<string>();
+            var channel2Events = new List<string>();
+
+            sut.Subscribe("channel1", x =>
+            {
+                channel1Events.Add(x);
+            });
+
+            sut.Subscribe("channel1", x =>
+            {
+                channel1Events.Add(x);
+            });
+
+            sut.Subscribe("channel2", x =>
+            {
+                channel2Events.Add(x);
+            });
+
+            sut.Publish("channel1", "1", true);
+            sut.Publish("channel1", "2", true);
+            sut.Publish("channel1", "3", false);
+
+            sut.Publish("channel2", "a", true);
+            sut.Publish("channel2", "b", true);
+
+            Assert.Equal(new[] { "1", "1", "2", "2" }, channel1Events.ToArray());
+            Assert.Equal(new[] { "a", "b" }, channel2Events.ToArray());
+
+        }
+    }
+}
