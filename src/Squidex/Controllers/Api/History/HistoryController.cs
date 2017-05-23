a3d25740374a3ccc81e3a74ae865599e192e1e51
Sebastian Stehle <sebastian@squidex.io>
2017-05-23 21:06:17 +0200

    Resharper warning fixed

--- src/Squidex/Controllers/Api/History/HistoryController.cs
+++ src/Squidex/Controllers/Api/History/HistoryController.cs
@@ -8,7 +8,6 @@
 
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
 using Squidex.Controllers.Api.History.Models;
