8d32e94b4e678a5d83434c94843fe785db443d5f
Sebastian Stehle <sebastian@squidex.io>
2017-07-05 18:50:07 +0200

    Benchmark structure

---
+++ tests/Benchmarks/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Benchmarks.Tests;
+
+namespace Benchmarks
+{
+    public static class Program
+    {
+        private static readonly List<IBenchmark> Benchmarks = new List<IBenchmark>
+        {
+            new AppendToEventStore()
+        };
+
+        public static void Main(string[] args)
+        {
+            var id = args.Length > 0 ? args[0] : string.Empty;
+
+            var benchmark = Benchmarks.Find(x => x.Id == id);
+
+            if (benchmark == null)
+            {
+                Console.WriteLine($"'{id}' is not a valid benchmark, please try: ");
+
+                var longestId = Benchmarks.Max(x => x.Id.Length);
+
+                foreach (var b in Benchmarks)
+                {
+                    Console.WriteLine($" * {b.Id.PadRight(longestId)}: {b.Name}");
+                }
+            }
+            else
+            {
+                const int numRuns = 3;
+
+                try
+                {
+                    var elapsed = 0d;
+                    var count = 0L;
+
+                    benchmark.Initialize();
+
+                    for (var run = 0; run < numRuns; run++)
+                    {
+                        try
+                        {
+                            benchmark.RunInitialize();
+
+                            var watch = Stopwatch.StartNew();
+
+                            count += benchmark.Run();
+
+                            watch.Stop();
+
+                            elapsed += watch.ElapsedMilliseconds;
+                        }
+                        finally
+                        {
+                            benchmark.RunCleanup();
+                        }
+                    }
+
+                    var totalElapsed = TimeSpan.FromMilliseconds(elapsed);
+                    var totalPerSecond = Math.Round(count / totalElapsed.TotalSeconds, 2);
+
+                    Console.WriteLine($"{benchmark.Name} completed after {totalElapsed}, {totalPerSecond} items/s");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Benchmark failed with '{e.Message}'");
+                }
+                finally
+                {
+                    benchmark.Cleanup();
+                }
+            }
+        }
+    }
+}
---
+++ tests/Benchmarks/Tests/AppendToEventStore.cs
@@ -0,0 +1,84 @@
+﻿// ==========================================================================
+//  AppendToEventStore.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using System;
+using MongoDB.Driver;
+using NodaTime;
+using Squidex.Infrastructure;
+using Squidex.Infrastructure.CQRS.Events;
+using Squidex.Infrastructure.MongoDb.EventStore;
+
+namespace Benchmarks.Tests
+{
+    public sealed class AppendToEventStore : IBenchmark
+    {
+        private IMongoClient mongoClient;
+        private IMongoDatabase mongoDatabase;
+
+        private static readonly EventData EventData = new EventData
+        {
+            EventId = Guid.NewGuid(),
+            Metadata = "EventMetdata",
+            Payload = "EventPayload",
+            Type = "MyEvent"
+        };
+
+        public string Id
+        {
+            get { return "appendToEventStore"; }
+        }
+
+        public string Name
+        {
+            get { return "Append Events to EventStore"; }
+        }
+
+        public void Initialize()
+        {
+            mongoClient = new MongoClient("mongodb://localhost");
+        }
+
+        public void RunInitialize()
+        {
+            mongoDatabase = mongoClient.GetDatabase(Guid.NewGuid().ToString());
+            mongoDatabase.CreateCollection("Test");
+        }
+
+        public long Run()
+        {
+            const long numCommits = 10;
+            const long eventStreams = 10;
+
+            var eventStore = new MongoEventStore(mongoDatabase, new DefaultEventNotifier(new InMemoryPubSub()), SystemClock.Instance);
+
+            for (var streamId = 0; streamId < eventStreams; streamId++)
+            {
+                var eventOffset = -1;
+                var streamName = streamId.ToString();
+
+                for (var commitId = 0; commitId < numCommits; commitId++)
+                {
+                    eventStore.AppendEventsAsync(Guid.NewGuid(), streamName, eventOffset, new[] { EventData }).Wait();
+
+                    eventOffset++;
+                }
+            }
+
+            return numCommits * eventStreams;
+        }
+
+        public void RunCleanup()
+        {
+            mongoClient.DropDatabase(mongoDatabase.DatabaseNamespace.DatabaseName);
+        }
+
+        public void Cleanup()
+        {
+        }
+    }
+}
