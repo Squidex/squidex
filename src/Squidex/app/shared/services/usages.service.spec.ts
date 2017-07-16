2208e6b5d47426543b7d349cc2b32418344ddd2b
Sebastian Stehle <sebastian@squidex.io>
2017-07-16 10:36:44 +0200

    Some tests brought back.

---
+++ src/Squidex/app/shared/services/usages.service.spec.ts
@@ -0,0 +1,143 @@
+/*
+ * Squidex Headless CMS
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved
+ */
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+
+import {
+    ApiUrlConfig,
+    CallsUsageDto,
+    CurrentCallsDto,
+    CurrentStorageDto,
+    DateTime,
+    StorageUsageDto,
+    UsagesService
+} from './../';
+
+describe('UsagesService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            providers: [
+                UsagesService,
+                { provide: ApiUrlConfig, useValue: new ApiUrlConfig('http://service/p/') }
+            ]
+        });
+    });
+
+    afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+        httpMock.verify();
+    }));
+
+    it('should make get request to get calls usages',
+        inject([UsagesService, HttpTestingController], (usagesService: UsagesService, httpMock: HttpTestingController) => {
+
+        let usages: CallsUsageDto[] | null = null;
+
+        usagesService.getCallsUsages('my-app', DateTime.parseISO_UTC('2017-10-12'), DateTime.parseISO_UTC('2017-10-13')).subscribe(result => {
+            usages = result;
+        });
+
+        const req = httpMock.expectOne('http://service/p/api/apps/my-app/usages/calls/2017-10-12/2017-10-13');
+
+        expect(req.request.method).toEqual('GET');
+        expect(req.request.headers.get('If-Match')).toBeNull();
+
+        req.flush([
+            {
+                date: '2017-10-12',
+                count: 10,
+                averageMs: 130
+            },
+            {
+                date: '2017-10-13',
+                count: 13,
+                averageMs: 170
+            }
+        ]);
+
+        expect(usages).toEqual(
+            [
+                new CallsUsageDto(DateTime.parseISO_UTC('2017-10-12'), 10, 130),
+                new CallsUsageDto(DateTime.parseISO_UTC('2017-10-13'), 13, 170)
+            ]);
+    }));
+
+    it('should make get request to get month calls',
+        inject([UsagesService, HttpTestingController], (usagesService: UsagesService, httpMock: HttpTestingController) => {
+
+        let usages: CurrentCallsDto | null = null;
+
+        usagesService.getMonthCalls('my-app').subscribe(result => {
+            usages = result;
+        });
+
+        const req = httpMock.expectOne('http://service/p/api/apps/my-app/usages/calls/month');
+
+        expect(req.request.method).toEqual('GET');
+        expect(req.request.headers.get('If-Match')).toBeNull();
+
+        req.flush({ count: 130, maxAllowed: 150 });
+
+        expect(usages).toEqual(new CurrentCallsDto(130, 150));
+    }));
+
+    it('should make get request to get storage usages',
+        inject([UsagesService, HttpTestingController], (usagesService: UsagesService, httpMock: HttpTestingController) => {
+
+        let usages: StorageUsageDto[] | null = null;
+
+        usagesService.getStorageUsages('my-app', DateTime.parseISO_UTC('2017-10-12'), DateTime.parseISO_UTC('2017-10-13')).subscribe(result => {
+            usages = result;
+        });
+
+        const req = httpMock.expectOne('http://service/p/api/apps/my-app/usages/storage/2017-10-12/2017-10-13');
+
+        expect(req.request.method).toEqual('GET');
+        expect(req.request.headers.get('If-Match')).toBeNull();
+
+        req.flush([
+            {
+                date: '2017-10-12',
+                count: 10,
+                size: 130
+            },
+            {
+                date: '2017-10-13',
+                count: 13,
+                size: 170
+            }
+        ]);
+
+        expect(usages).toEqual(
+            [
+                new StorageUsageDto(DateTime.parseISO_UTC('2017-10-12'), 10, 130),
+                new StorageUsageDto(DateTime.parseISO_UTC('2017-10-13'), 13, 170)
+            ]);
+    }));
+
+    it('should make get request to get today storage',
+        inject([UsagesService, HttpTestingController], (usagesService: UsagesService, httpMock: HttpTestingController) => {
+
+        let usages: CurrentStorageDto | null = null;
+
+        usagesService.getTodayStorage('my-app').subscribe(result => {
+            usages = result;
+        });
+
+        const req = httpMock.expectOne('http://service/p/api/apps/my-app/usages/storage/today');
+
+        expect(req.request.method).toEqual('GET');
+        expect(req.request.headers.get('If-Match')).toBeNull();
+
+        req.flush({ size: 130, maxAllowed: 150 });
+
+        expect(usages).toEqual(new CurrentStorageDto(130, 150));
+    }));
+});
---
+++ src/Squidex/app/shared/services/users-provider.service.spec.ts
@@ -0,0 +1,103 @@
+/*
+ * Squidex Headless CMS
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved
+ */
+
+import { Observable } from 'rxjs';
+import { IMock, Mock, Times } from 'typemoq';
+
+import {
+    AuthService,
+    Profile,
+    UserDto,
+    UsersProviderService,
+    UsersService
+} from './../';
+
+describe('UsersProviderService', () => {
+    let authService: IMock<AuthService>;
+    let usersService: IMock<UsersService>;
+    let usersProviderService: UsersProviderService;
+
+    beforeEach(() => {
+        authService = Mock.ofType(AuthService);
+        usersService = Mock.ofType(UsersService);
+        usersProviderService = new UsersProviderService(usersService.object, authService.object);
+    });
+
+    it('Should return users service when user not cached', () => {
+        const user = new UserDto('123', 'mail@domain.com', 'User1', 'path/to/image', true);
+
+        usersService.setup(x => x.getUser('123'))
+            .returns(() => Observable.of(user)).verifiable(Times.once());
+
+        let resultingUser: UserDto | null = null;
+
+        usersProviderService.getUser('123').subscribe(result => {
+            resultingUser = result;
+        }).unsubscribe();
+
+        expect(resultingUser).toBe(user);
+
+        usersService.verifyAll();
+    });
+
+    it('Should return provide user from cache', () => {
+        const user = new UserDto('123', 'mail@domain.com', 'User1', 'path/to/image', true);
+
+        usersService.setup(x => x.getUser('123'))
+            .returns(() => Observable.of(user)).verifiable(Times.once());
+
+        usersProviderService.getUser('123');
+
+        let resultingUser: UserDto | null = null;
+
+        usersProviderService.getUser('123').subscribe(result => {
+            resultingUser = result;
+        }).unsubscribe();
+
+        expect(resultingUser).toBe(user);
+
+        usersService.verifyAll();
+    });
+
+    it('Should return me when user is current user', () => {
+        const user = new UserDto('123', 'mail@domain.com', 'User1', 'path/to/image', true);
+
+        authService.setup(x => x.user)
+            .returns(() => new Profile(<any>{ profile: { sub: '123'}}));
+
+        usersService.setup(x => x.getUser('123'))
+            .returns(() => Observable.of(user)).verifiable(Times.once());
+
+        let resultingUser: UserDto | null = null;
+
+        usersProviderService.getUser('123').subscribe(result => {
+            resultingUser = result;
+        }).unsubscribe();
+
+        expect(resultingUser).toEqual(new UserDto('123', 'mail@domain.com', 'Me', 'path/to/image', true));
+
+        usersService.verifyAll();
+    });
+
+    it('Should return invalid user when not found', () => {
+        authService.setup(x => x.user)
+            .returns(() => new Profile(<any>{ profile: { sub: '123'}}));
+
+        usersService.setup(x => x.getUser('123'))
+            .returns(() => Observable.throw('NOT FOUND')).verifiable(Times.once());
+
+        let resultingUser: UserDto | null = null;
+
+        usersProviderService.getUser('123').subscribe(result => {
+            resultingUser = result;
+        }).unsubscribe();
+
+        expect(resultingUser).toEqual(new UserDto('NOT FOUND', 'NOT FOUND', 'NOT FOUND', null, false));
+
+        usersService.verifyAll();
+    });
+});
