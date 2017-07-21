0674bb4bdf1fa5fda9fb3d265ba4b2bc144ab87e
pushrbx <thomace44@gmail.com>
2017-07-21 11:12:43 +0100

    added azure blob storage asset store tests

---
+++ tests/Squidex.Infrastructure.Tests/Assets/AzureBlobAssetStoreTests.cs
@@ -0,0 +1,27 @@
+﻿// ==========================================================================
+//  AzureBlobAssetStoreTests.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using Squidex.Infrastructure.Azure.Storage;
+
+namespace Squidex.Infrastructure.Assets
+{
+    public class AzureBlobAssetStoreTests : AssetStoreTests<AzureBlobAssetStore>
+    {
+        public override AzureBlobAssetStore CreateStore()
+        {
+            var azureStorageAccount =
+                new StorageAccountManager("UseDevelopmentStorage=true");
+
+            return new AzureBlobAssetStore(azureStorageAccount, "squidex-test-container"); ;
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
