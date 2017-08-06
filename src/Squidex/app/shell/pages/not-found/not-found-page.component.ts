500b52d17c26b86954365e9a077e89f45940b9a1
Sebastian Stehle <sebastian@squidex.io>
2017-08-06 12:34:10 +0200

    404 back improved.

--- src/Squidex/app/shell/pages/not-found/not-found-page.component.ts
+++ src/Squidex/app/shell/pages/not-found/not-found-page.component.ts
@@ -6,10 +6,20 @@
  */
 
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'sqx-not-found-page',
     styleUrls: ['./not-found-page.component.scss'],
     templateUrl: './not-found-page.component.html'
 })
-export class NotFoundPageComponent { }
+export class NotFoundPageComponent {
+    constructor(
+        private readonly location: Location
+    ) {
+    }
+
+    public back() {
+        this.location.back();
+    }
+}
