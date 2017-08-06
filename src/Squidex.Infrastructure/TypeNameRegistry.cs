6482f313487a1e92618517e3dd8026b830a87eea
Sebastian Stehle <sebastian@squidex.io>
2017-08-06 20:48:41 +0200

    Auto cleanup on failure.

--- src/Squidex.Infrastructure/TypeNameRegistry.cs
+++ src/Squidex.Infrastructure/TypeNameRegistry.cs
@@ -78,7 +78,7 @@ namespace Squidex.Infrastructure
             {
                 var typeNameAttribute = type.GetTypeInfo().GetCustomAttribute<TypeNameAttribute>();
 
-                if (typeNameAttribute != null && !string.IsNullOrWhiteSpace(typeNameAttribute.TypeName))
+                if (!string.IsNullOrWhiteSpace(typeNameAttribute?.TypeName))
                 {
                     Map(type, typeNameAttribute.TypeName);
                 }
