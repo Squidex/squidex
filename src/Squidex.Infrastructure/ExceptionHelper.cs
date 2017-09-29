44ca50cb7d65bca13983def695844e311a6daf98
Sebastian Stehle <sebastian@squidex.io>
2017-09-29 20:20:34 +0200

    File was not added

---
+++ src/Squidex.Infrastructure/ExceptionHelper.cs
@@ -0,0 +1,27 @@
+﻿// ==========================================================================
+//  ExceptionHelper.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using System;
+
+namespace Squidex.Infrastructure
+{
+    public static class ExceptionHelper
+    {
+        public static bool Is<T>(this Exception ex) where T : Exception
+        {
+            if (ex is AggregateException aggregateException)
+            {
+                aggregateException = aggregateException.Flatten();
+
+                return aggregateException.InnerExceptions.Count == 1 && Is<T>(aggregateException.InnerExceptions[0]);
+            }
+
+            return ex is OperationCanceledException;
+        }
+    }
+}
