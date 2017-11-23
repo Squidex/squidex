e4651272a9ab4ed20873c9258b17e28bac9df0d4
Sebastian Stehle <sebastian@squidex.io>
2017-11-23 19:25:19 +0100

    LRUCache

---
+++ src/Squidex.Infrastructure/Caching/LRUCacheItem.cs
@@ -0,0 +1,18 @@
+﻿// ==========================================================================
+//  LRUCacheItem.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+#pragma warning disable SA1401 // Fields must be private
+
+namespace Squidex.Infrastructure.Caching
+{
+    internal class LRUCacheItem
+    {
+        public object Key;
+        public object Value;
+    }
+}
