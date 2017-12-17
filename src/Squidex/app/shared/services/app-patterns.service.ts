71f9051ffe5d64ed0a8d4124cf0829f42a0ec3f4
Sebastian Stehle <sebastian@squidex.io>
2017-12-17 14:33:06 +0100

    Service added

---
+++ src/Squidex/app/shared/services/app-patterns.service.ts
@@ -0,0 +1,127 @@
+/*
+ * Squidex Headless CMS
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved
+ */
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import 'framework/angular/http-extensions';
+
+import {
+    ApiUrlConfig,
+    HTTP,
+    Version
+} from 'framework';
+
+export class AppPatternsDto {
+    constructor(
+        public readonly patterns: AppPatternDto[],
+        public readonly version: Version
+    ) {
+    }
+
+    public addPattern(pattern: AppPatternDto, version: Version) {
+        return new AppPatternsDto([...this.patterns, pattern],  version);
+    }
+
+    public updatePattern(pattern: AppPatternDto, version: Version) {
+        return new AppPatternsDto(this.patterns.map(p => p.patternId === pattern.patternId ? pattern : p), version);
+    }
+
+    public removePattern(pattern: AppPatternDto, version: Version) {
+        return new AppPatternsDto(this.patterns.filter(c => c.patternId !== pattern.patternId), version);
+    }
+}
+
+export class AppPatternDto {
+    constructor(
+        public readonly patternId: string,
+        public readonly name: string,
+        public readonly pattern: string,
+        public readonly message: string
+    ) {
+    }
+
+    public update(update: UpdatePatternDto) {
+        return new AppPatternDto(
+            this.patternId,
+            update.name,
+            update.pattern,
+            update.message);
+    }
+}
+
+export class UpdatePatternDto {
+    constructor(
+        public readonly name: string,
+        public readonly pattern: string,
+        public readonly message: string
+    ) {
+    }
+}
+
+
+@Injectable()
+export class AppPatternsService {
+    constructor(
+        private readonly http: HttpClient,
+        private readonly apiUrl: ApiUrlConfig
+    ) {
+    }
+
+    public getPatterns(appName: string): Observable<AppPatternsDto> {
+        const url = this.apiUrl.buildUrl(`api/apps/${appName}/patterns`);
+
+        return HTTP.getVersioned<any>(this.http, url)
+            .map(response => {
+                const body = response.payload.body;
+
+                const items: any[] = body;
+
+                return new AppPatternsDto(
+                    items.map(item => {
+                        return new AppPatternDto(
+                            item.patternId,
+                            item.name,
+                            item.pattern,
+                            item.message);
+                    }),
+                    response.version);
+            })
+            .pretifyError('Failed to add pattern. Please reload.');
+    }
+
+    public postPattern(appName: string, pattern: UpdatePatternDto, version: Version): Observable<AppPatternDto> {
+        const url = this.apiUrl.buildUrl(`api/apps/${appName}/patterns`);
+
+        return HTTP.postVersioned<any>(this.http, url, pattern, version)
+            .map(response => {
+                const body = response.payload.body;
+
+                return new AppPatternDto(
+                    body.patternId,
+                    body.name,
+                    body.pattern,
+                    body.message);
+            })
+            .pretifyError('Failed to add pattern. Please reload.');
+    }
+
+    public updatePattern(appName: string, id: string, pattern: UpdatePatternDto, version: Version): Observable<any> {
+        const url = this.apiUrl.buildUrl(`api/apps/${appName}/patterns/${id}`);
+
+        return HTTP.putVersioned(this.http, url, pattern, version)
+            .pretifyError('Failed to update pattern. Please reload.');
+    }
+
+    public deletePattern(appName: string, id: string, version: Version): Observable<AppPatternDto> {
+        const url = this.apiUrl.buildUrl(`api/apps/${appName}/patterns/${id}`);
+
+        return HTTP.deleteVersioned(this.http, url, version)
+            .pretifyError('Failed to remove pattern. Please reload.');
+    }
+}
