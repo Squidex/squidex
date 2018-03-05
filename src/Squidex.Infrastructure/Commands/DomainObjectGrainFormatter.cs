56ef76d6277fbf1152ffa4ebc030fd2faf373f2b
Sebastian Stehle <mail2stehle@gmail.com>
2018-03-05 11:31:39 +0100

    File added.

---
+++ src/Squidex.Infrastructure/Commands/DomainObjectGrainFormatter.cs
@@ -0,0 +1,32 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+using Orleans;
+
+namespace Squidex.Infrastructure.Commands
+{
+    public static class DomainObjectGrainFormatter
+    {
+        public static string Format(IGrainCallContext context)
+        {
+            if (context.Method == null)
+            {
+                return "Unknown";
+            }
+
+            if (string.Equals(context.Method.Name, nameof(IDomainObjectGrain.ExecuteAsync), StringComparison.CurrentCultureIgnoreCase) &&
+                context.Arguments?.Length == 1 &&
+                context.Arguments[0] != null)
+            {
+                return context.Arguments[0].ToString();
+            }
+
+            return context.Method.Name;
+        }
+    }
+}
