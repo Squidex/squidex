9867dbf2492485808fe29f78268135fb3cf8ec2b
Sebastian Stehle <sebastian@squidex.io>
2018-04-14 18:25:14 +0200

    Another test

--- src/Squidex/app-config/karma.coverage.conf.js
+++ src/Squidex/app-config/karma.coverage.conf.js
@@ -1,5 +1,7 @@
 ﻿var webpackConfig = require('./webpack.test.coverage');
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
     var _config = {
         /** 
