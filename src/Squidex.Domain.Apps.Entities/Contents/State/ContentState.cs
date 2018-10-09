03a6f5a99c55faeadceed8a4001bb1e8326f7f1e
Avd6977 <avd6977@gmail.com>
2018-10-09 08:07:02 -0500

    Pending should be false when changing status
    Fixing a weird state on content
    1. Create and publish a content item
    2. Make a pending change
    3. Select "Unpublish" and the pending data is still there.
    
    If you're unpublished, you shouldn't be able to have a pending version.

--- src/Squidex.Domain.Apps.Entities/Contents/State/ContentState.cs
+++ src/Squidex.Domain.Apps.Entities/Contents/State/ContentState.cs
@@ -86,6 +86,7 @@ namespace Squidex.Domain.Apps.Entities.Contents.State
         protected void On(ContentStatusChanged @event)
         {
             ScheduleJob = null;
+            IsPending = false;
 
             Status = @event.Status;
 
