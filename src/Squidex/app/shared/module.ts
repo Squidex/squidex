ba2fa8d28d90e571f9737591b0f743d6e9df11b0
Sebastian Stehle <sebastian@squidex.io>
2018-10-21 15:19:10 +0200

    Build fix.

--- src/Squidex/app/shared/module.ts
+++ src/Squidex/app/shared/module.ts
@@ -172,7 +172,6 @@ export class SqxSharedModule {
                 BackupsState,
                 ClientsState,
                 CommentsService,
-                CommentsState,
                 ContentMustExistGuard,
                 ContentsService,
                 ContentsState,
