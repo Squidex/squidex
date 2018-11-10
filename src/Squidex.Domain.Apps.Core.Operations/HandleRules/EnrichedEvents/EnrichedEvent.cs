f2001dfb00f02c41debf028a006a988dbb2b2750
Sebastian Stehle <sebastian@squidex.io>
2018-11-10 17:57:38 +0100

    Add event to serialization.

--- src/Squidex.Domain.Apps.Core.Operations/HandleRules/EnrichedEvents/EnrichedEvent.cs
+++ src/Squidex.Domain.Apps.Core.Operations/HandleRules/EnrichedEvents/EnrichedEvent.cs
@@ -15,19 +15,18 @@ namespace Squidex.Domain.Apps.Core.HandleRules.EnrichedEvents
 {
     public abstract class EnrichedEvent
     {
-        [JsonIgnore]
-        public abstract Guid AggregateId { get; }
-
         public NamedId<Guid> AppId { get; set; }
 
         public RefToken Actor { get; set; }
 
         public Instant Timestamp { get; set; }
 
+        public string Name { get; set; }
+
         public long Version { get; set; }
 
         [JsonIgnore]
-        public string Name { get; set; }
+        public abstract Guid AggregateId { get; }
 
         [JsonIgnore]
         public IUser User { get; set; }
