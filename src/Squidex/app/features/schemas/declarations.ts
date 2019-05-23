bae480a3bd42c5092e0011fb44e8d071891304a3
Sebastian Stehle <sebastian@squidex.io>
2019-05-23 17:38:54 +0200

    Share component.

--- src/Squidex/app/features/schemas/declarations.ts
+++ src/Squidex/app/features/schemas/declarations.ts
@@ -26,6 +26,7 @@ export * from './pages/schema/types/tags-validation.component';
 export * from './pages/schema/forms/field-form-common.component';
 export * from './pages/schema/forms/field-form-ui.component';
 export * from './pages/schema/forms/field-form-validation.component';
+export * from './pages/schema/forms/field-form.component';
 
 export * from './pages/schema/field.component';
 export * from './pages/schema/field-wizard.component';
