87db8a5b1a4d9b60100ae2ff14010b44958b0341
Jason Segal <jason.segal@reedbusiness.com>
2019-05-31 22:00:05 +0100

    logging for ciBuild script for debugging

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -6,7 +6,12 @@ param(
 )
 
 $TAG_NAME = .\build_tools\generate_gitversion.ps1 -BuildCounter $BuildCounter
+
+Write-Host $TAG_NAME
+
 $DOCKER_TAG =  $TAG_NAME.toLower() -replace "[^a-z0-9_\-]",'.'
+
+Write-Host $DOCKER_TAG
 
 docker build . -t $DOCKER_TAG --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
