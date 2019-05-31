028977def2e556437033c5696d20b0e227c1e45f
saurav vijay <saurav.vijay@reedbusiness.com>
2019-05-31 10:12:41 +0100

    VEGA-101 updated ci script

--- build_tools/ciBuild.ps1
+++ build_tools/ciBuild.ps1
@@ -1,4 +1,11 @@
-$TAG_NAME="nexus.cha.rbxd.ds:8000/cosmos:test-tag"
+param(
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $BuildCounter
+)
+
+$TAG_NAME= .\build_tools\generate_gitversion.ps1 -BuildCounter $BuildCounter
 docker build . -t $TAG_NAME --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
