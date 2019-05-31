07da16ce0b8f3572cdb92f36aaa3bf8b508990b2
Jason Segal <jason.segal@reedbusiness.com>
2019-06-01 00:42:43 +0100

    added repo to tag after removal of invalid characters (repo requires a '/')

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -7,12 +7,9 @@ param(
 
 $GIT_VERSION_PS = "build_tools/generate_gitversion.ps1 -BuildCounter $BuildCounter"
 Invoke-Expression $GIT_VERSION_PS
-$TAG_NAME = $env:Version
 
-
-Write-Host $TAG_NAME
-
-$DOCKER_TAG =  "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME.toLower() -replace "[^a-z0-9_\-]",'.'
+$TAG_NAME = $env:Version.toLower() -replace "[^a-z0-9_\-]",'.'
+$DOCKER_TAG =  "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME
 
 Write-Host $DOCKER_TAG
 
