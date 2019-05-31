6041b462adc81d9679e8baafe583434a9378d8ed
Jason Segal <jason.segal@reedbusiness.com>
2019-05-31 07:54:08 +0100

    Converted script from shell to powershell

--- ciBuild.sh
+++
@@ -1,6 +0,0 @@
-TAG_NAME="nexus.cha.rbxd.ds:8000/cosmos:test-tag"
-docker build -t $TAG_NAME --pull \
-    --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 \
-    --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 \
-    -f Dockerfile .
-docker push $TAG_NAME
