9ed2cec622c1f2e5c356891a0e2a6310e50895d7
Jason Segal <jason.segal@reedbusiness.com>
2019-05-31 21:54:19 +0100

    convert docker tag toLower() in ciBuild script

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $TAG_NAME = .\build_tools\generate_gitversion.ps1 -BuildCounter $BuildCounter
-$DOCKER_TAG =  $TAG_NAME -replace "[^a-z0-9_\-]",'.'
+$DOCKER_TAG =  $TAG_NAME.toLower() -replace "[^a-z0-9_\-]",'.'
 
 docker build . -t $DOCKER_TAG --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
