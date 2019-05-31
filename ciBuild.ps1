b437bb55ab287b6a16abfa913c93ba3d4d592fe6
Jason Segal <jason.segal@reedbusiness.com>
2019-05-31 21:51:48 +0100

    convert to docker tag from name tag

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -5,10 +5,11 @@ param(
     $BuildCounter
 )
 
-$TAG_NAME= .\build_tools\generate_gitversion.ps1 -BuildCounter $BuildCounter
-Write-Host $TAG_NAME.ToLower()
-docker build . -t $TAG_NAME --pull `
+$TAG_NAME = .\build_tools\generate_gitversion.ps1 -BuildCounter $BuildCounter
+$DOCKER_TAG =  $TAG_NAME -replace "[^a-z0-9_\-]",'.'
+
+docker build . -t $DOCKER_TAG --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     -f Dockerfile
-docker push $TAG_NAME
+docker push $DOCKER_TAG
