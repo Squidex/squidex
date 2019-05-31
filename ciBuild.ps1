7c31e70ee7777829c3e9ece3c589fcc45fd90afc
Jason Segal <jason.segal@reedbusiness.com>
2019-05-31 21:33:06 +0100

    removed repo name from ciBuild script

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -10,4 +10,4 @@ docker build . -t $TAG_NAME --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     -f Dockerfile
-docker push "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME
+docker push $TAG_NAME
