7f552790f8d3b630a8751f2222886fedcb9d799c
Jason Segal <jason.segal@reedbusiness.com>
2019-06-01 16:26:02 +0100

    Removed unnecessary logging and added comments to ciBuild script

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -5,16 +5,20 @@ param(
     $BuildCounter
 )
 
+# Exexute gitversion.exe file to calculate the full semantic version for the build (saved in $env:Version)
+# Format of the version is [semantic version].[branch name].[branch build count]
 $GIT_VERSION_PS = "build_tools/generate_gitversion.ps1 -BuildCounter $BuildCounter"
 Invoke-Expression $GIT_VERSION_PS
 
+# Convert the semantic version to lowercase and remove all breaking characters
 $TAG_NAME = $env:Version.toLower() -replace "[^a-z0-9_\-]",'.'
-$DOCKER_TAG =  "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME
 
-Write-Host $DOCKER_TAG
+# Add the repository to the full semantic version
+$DOCKER_TAG =  "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME
 
 docker build . -t $DOCKER_TAG --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     -f Dockerfile
+
 docker push $DOCKER_TAG
