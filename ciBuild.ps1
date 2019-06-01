fbc8fcdcc611117957f947e4b60308a41502af68
Jason Segal <jason.segal@reedbusiness.com>
2019-06-01 16:57:07 +0100

    Removed dockerfile tag

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -1,5 +1,4 @@
 param(
-
     [Parameter(Mandatory=$false)]
     [Int]
     $BuildCounter
@@ -19,6 +18,5 @@ $DOCKER_TAG =  "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME
 docker build . -t $DOCKER_TAG --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
-    -f Dockerfile
 
 # docker push $DOCKER_TAG
