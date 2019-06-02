45263888a84c3b7d5a7e07ac6159fd8064109b0e
Jason Segal <jason.segal@reedbusiness.com>
2019-06-02 15:06:20 +0100

    Added repo path as variable (removed code replication)
    Added latest tag
    Uncommented docker push

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -9,14 +9,18 @@ param(
 $GIT_VERSION_PS = "build_tools/generate_gitversion.ps1 -BuildCounter $BuildCounter"
 Invoke-Expression $GIT_VERSION_PS
 
+$REPO_PATH = "nexus.cha.rbxd.ds:8000/cosmos"
+
 # Convert the semantic version to lowercase and remove all breaking characters
 $TAG_NAME = $env:Version.toLower() -replace "[^a-z0-9_\-]",'.'
 
-# Add the repository to the full semantic version
-$DOCKER_TAG =  "nexus.cha.rbxd.ds:8000/cosmos:" + $TAG_NAME
+# Build the docker tags
+$SEMANTIC_DOCKER_TAG = $REPO_PATH + ":" + $TAG_NAME
+$LATEST_DOCKER_TAG = $REPO_PATH + ":" + "latest"
 
-docker build . -t $DOCKER_TAG --pull `
+docker build . -t $LATEST_DOCKER_TAG -t $SEMANTIC_DOCKER_TAG --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
 
-# docker push $DOCKER_TAG
+docker push $LATEST_DOCKER_TAG
+docker push $SEMANTIC_DOCKER_TAG
