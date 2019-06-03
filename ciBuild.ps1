5e2c64702c11d5be40a093feb5055a8a8d492e3a
Jason Segal <jason.segal@reedbusiness.com>
2019-06-03 12:01:26 +0100

    renamed variables to use camelcase like other scripts

--- ciBuild.ps1
+++ ciBuild.ps1
@@ -6,18 +6,18 @@ param(
 
 # Exexute gitversion.exe file to calculate the full semantic version for the build (saved in $env:Version)
 # Format of the version is [semantic version].[branch name].[branch build count]
-$GIT_VERSION_PS = "build_tools/generate_gitversion.ps1 -BuildCounter $BuildCounter"
-Invoke-Expression $GIT_VERSION_PS
+$gitVersionPs = "build_tools/generate_gitversion.ps1 -BuildCounter $BuildCounter"
+Invoke-Expression $gitVersionPs
 
-$REPO_PATH = "nexus.cha.rbxd.ds:8000/cosmos"
-$TAG_NAME = $env:Version
+$repoPath = "nexus.cha.rbxd.ds:8000/cosmos"
+$tagName = $env:Version
 
-$SEMANTIC_DOCKER_TAG = $REPO_PATH + ":" + $TAG_NAME
+$semanticDockerTag = $repoPath + ":" + $tagName
 
-Write-Host "Building docker image $SEMANTIC_DOCKER_TAG"
-docker build . -t $SEMANTIC_DOCKER_TAG --pull `
+Write-Host "Building docker image $semanticDockerTag"
+docker build . -t $semanticDockerTag --pull `
     --build-arg http_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
     --build-arg https_proxy=http://outboundproxycha.cha.rbxd.ds:3128 `
 
-Write-Host "Pushing docker image $SEMANTIC_DOCKER_TAG"
-docker push $SEMANTIC_DOCKER_TAG
+Write-Host "Pushing docker image $semanticDockerTag"
+docker push $semanticDockerTag
