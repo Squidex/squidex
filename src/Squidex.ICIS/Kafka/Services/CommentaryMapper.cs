2bf30a8b7f4b953400cc05dd980ae98459bd1aca
saurav vijay <saurav.vijay@reedbusiness.com>
2019-06-25 13:00:56 +0100

    VEGA-82 Fixed bug VEGA-192

--- src/Squidex.ICIS/Kafka/Services/CommentaryMapper.cs
+++ src/Squidex.ICIS/Kafka/Services/CommentaryMapper.cs
@@ -5,6 +5,8 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Avro.Specific;
@@ -14,6 +16,7 @@ using Squidex.Domain.Apps.Entities;
 using Squidex.Domain.Apps.Entities.Apps;
 using Squidex.Domain.Apps.Entities.Contents;
 using Squidex.Domain.Apps.Entities.Contents.Repositories;
+using Squidex.Domain.Apps.Entities.Schemas;
 using Squidex.ICIS.Actions.Kafka.Entities;
 using Squidex.Infrastructure.Json.Objects;
 
@@ -21,6 +24,15 @@ namespace Squidex.ICIS.Actions.Kafka
 {
     public class CommentaryMapper : IKafkaMessageMapper
     {
+        private IContentRepository contentRepository;
+        private readonly IAppEntity commentaryApp;
+
+        public CommentaryMapper(IAppEntity commentaryApp, IContentRepository contentRepository)
+        {
+            this.commentaryApp = commentaryApp;
+            this.contentRepository = contentRepository;
+        }
+
         public ISpecificRecord ToAvro(EnrichedContentEvent contentEvent)
         {
             var commentary = new Commentary();
@@ -36,18 +48,44 @@ namespace Squidex.ICIS.Actions.Kafka
 
             if (!contentEvent.Data.TryGetValue("CommentaryType", out var commentaryTypeData))
             {
-                throw new System.Exception("Unable to find CommentaryType field.");
+                throw new Exception("Unable to find CommentaryType field.");
             }
 
             if (!contentEvent.Data.TryGetValue("Commodity", out var commodityData))
             {
-                throw new System.Exception("Unable to find Commodity field.");
+                throw new Exception("Unable to find Commodity field.");
             }
 
-            commentary.CommentaryTypeId = ((Collection<IJsonValue>)commentaryTypeData["iv"])[0].ToString();
-            commentary.CommodityId = ((Collection<IJsonValue>)commodityData["iv"])[0].ToString();
+            var commentaryTypeMongoId = ((Collection<IJsonValue>)commentaryTypeData["iv"])[0].ToString();
+            var commodityMongoId = ((Collection<IJsonValue>)commodityData["iv"])[0].ToString();
+
+            var publishedEntities = GetPublishedEntities(string.Join(',', new[] { commodityMongoId, commentaryTypeMongoId }));
+
+            var commentaryType = publishedEntities.Find(x => x.Item2.SchemaDef.Name.Equals("commentary-type")).Item1;
+            var commodity = publishedEntities.Find(x => x.Item2.SchemaDef.Name.Equals("commodity")).Item1;
+
+            if (!commentaryType.Data.TryGetValue("ID", out var commentaryTypeIdData))
+            {
+                throw new Exception("Unable to find commentary-type Id field.");
+            }
+
+            if (!commodity.Data.TryGetValue("ID", out var commodityIdData))
+            {
+                throw new Exception("Unable to find commodity Id field.");
+            }
+
+            commentary.CommentaryTypeId = commentaryTypeIdData["iv"].ToString();
+            commentary.CommodityId = commodityIdData["iv"].ToString();
 
             return commentary;
+        }
+
+        private List<(IContentEntity, ISchemaEntity)> GetPublishedEntities(string entityIds)
+        {
+            var entity = this.contentRepository.QueryAsync(this.commentaryApp, new Status[] { Status.Published }, new HashSet<Guid>(Q.Empty.WithIds(entityIds).Ids), false);
+            entity.Wait();
+
+            return entity.Result;
         }
     }
 }
--- src/Squidex.ICIS/Kafka/Services/KafkaMessageFactory.cs
+++ src/Squidex.ICIS/Kafka/Services/KafkaMessageFactory.cs
@@ -10,19 +10,20 @@ using Avro.Specific;
 using Squidex.Domain.Apps.Core.Contents;
 using Squidex.Domain.Apps.Core.HandleRules.EnrichedEvents;
 using Squidex.Domain.Apps.Entities;
+using Squidex.Domain.Apps.Entities.Apps;
 using Squidex.Domain.Apps.Entities.Contents.Repositories;
 
 namespace Squidex.ICIS.Actions.Kafka
 {
     public static class KafkaMessageFactory
     {
-        public static ISpecificRecord GetKafkaMessage(string schemaName, EnrichedContentEvent contentEvent)
+        public static ISpecificRecord GetKafkaMessage(string schemaName, EnrichedContentEvent contentEvent, IAppEntity commentaryApp, IContentRepository contentRepository)
         {
             ISpecificRecord entity = null;
             switch (schemaName)
             {
                 case "commentary":
-                    entity = new CommentaryMapper().ToAvro(contentEvent);
+                    entity = new CommentaryMapper(commentaryApp, contentRepository).ToAvro(contentEvent);
                     break;
                 case "commentary-type":
                     entity = new CommentaryTypeMapper().ToAvro(contentEvent);
