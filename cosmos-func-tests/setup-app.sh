d20b4cdca5147b54b7e24f14c097442bfd635b97
saurav vijay <saurav.vijay@reedbusiness.com>
2019-07-02 17:23:40 +0100

    updated commands

--- cosmos-func-tests/setup-app.sh
+++ cosmos-func-tests/setup-app.sh
@@ -5,3 +5,6 @@ nohup node ./JSFiles/utils/Db.js &
 
 # dotnet app
 nohup dotnet run --project ../src/Squidex &
+
+# run deployment scipts
+waitfor https://localhost:5000/ && cd ../deploymentscripts && @powershell -NoProfile -ExecutionPolicy Unrestricted -Command ./Deployment.ps1 -identityServiceBaseUrl 'http://identityservice.systest.tesla.cha.rbxd.ds' -apiBaseUrl 'https://localhost:5000/api' -tokenUser 'CMSDeployer' -tokenPassword 'p@55w0rd' -appName 'commentary' 
