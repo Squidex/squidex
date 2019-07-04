8476e5bfc0bdb9bac33b0e31644fc5fa18bedb63
Sebastian <sebastian@squidex.io>
2019-07-04 21:39:52 +0200

    Performance improvements:
    1. Caching for app tags
    2. Caching for available languages.

--- src/Squidex.Domain.Apps.Core.Operations/Tags/TagsSet.cs
+++ src/Squidex.Domain.Apps.Core.Operations/Tags/TagsSet.cs
@@ -9,7 +9,18 @@ using System.Collections.Generic;
 
 namespace Squidex.Domain.Apps.Core.Tags
 {
-    public sealed class TagSet : Dictionary<string, Tag>
+    public sealed class TagsSet : Dictionary<string, int>
     {
+        public long Version { get; set; }
+
+        public TagsSet()
+        {
+        }
+
+        public TagsSet(IDictionary<string, int> tags, long version)
+            : base(tags)
+        {
+            Version = version;
+        }
     }
 }
