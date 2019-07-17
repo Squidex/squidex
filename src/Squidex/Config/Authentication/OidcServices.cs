7326df4219f517ab84bac51d6f1f0f635c5094ee
Sebastian Stehle <sebastian@squidex.io>
2019-07-17 18:25:44 +0200

    Code cleanup.

--- src/Squidex/Config/Authentication/OidcServices.cs
+++ src/Squidex/Config/Authentication/OidcServices.cs
@@ -8,8 +8,6 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.DependencyInjection;
-using Squidex.Infrastructure.Tasks;
-using Squidex.Web;
 
 namespace Squidex.Config.Authentication
 {
