e50039e098cbc85c7f8f972ca08a72b73c8e0f28
Sebastian Stehle <sebastian@squidex.io>
2019-07-28 19:48:00 +0200

    Jscript fix.

--- src/Squidex.Domain.Apps.Core.Operations/Scripting/JintScriptEngine.cs
+++ src/Squidex.Domain.Apps.Core.Operations/Scripting/JintScriptEngine.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using Esprima;
 using Jint;
 using Jint.Native;
 using Jint.Native.Date;
@@ -122,6 +123,10 @@ namespace Squidex.Domain.Apps.Core.Scripting
                 throw new ValidationException($"Failed to execute script with javascript syntax error: {ex.Message}", new ValidationError(ex.Message));
             }
             catch (JavaScriptException ex)
+            {
+                throw new ValidationException($"Failed to execute script with javascript error: {ex.Message}", new ValidationError(ex.Message));
+            }
+            catch (ParserException ex)
             {
                 throw new ValidationException($"Failed to execute script with javascript error: {ex.Message}", new ValidationError(ex.Message));
             }
