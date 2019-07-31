6b10419618201ff19786cd1aa643ccea43283630
pavani <pavani.ramakrishna1991@gmail.com>
2019-07-31 11:14:58 +0100

    removed Squidex home page code as the login page
    removed Squidex base page handling as we no longer need it
    uncommented lines of code in afterall() which were commented for testing

--- cosmos-func-tests/pages/LoginPage.po.ts
+++ cosmos-func-tests/pages/LoginPage.po.ts
@@ -1,37 +1,34 @@
+import { BrowserUtil } from "./../utils/Browser.util";
 import { LoginData } from "../data/LoginData";
-import {
-  browser,
-  element,
-  by,
-  ExpectedConditions,
-  ElementFinder,
-  $
-} from "protractor";
-import { BasePage } from "./BasePage.po";
+import { browser, element, by } from "protractor";
 import { config } from "../config";
 
 /**
  * Class representing login page.
  * Login window which opens after clicking on Login button on Squidex base page
  */
-export class LoginPage extends BasePage {
+export class LoginPage extends BrowserUtil {
   /**
    * signs in using specified username/password in login.json file.
    */
-  public login(loginData: LoginData) {
+
+  private browserPg = new BrowserUtil();
+
+  public async login(loginData: LoginData) {
+    this.browserPg.waitForAngularDisabledOnCurrentWindow();
+    this.browserPg.switchToChildWindow();
     const usernameInput = element(by.id("userNameInput"));
     const passwordInput = element(by.id("passwordInput"));
     const submitButton = element(by.id("submitButton"));
     usernameInput.sendKeys(loginData.username);
     passwordInput.sendKeys(loginData.password);
     submitButton.click();
+    this.browserPg.waitForAngularEnabledOnCurrentWindow().then(async () => {
+      this.browserPg.switchToParentWindow();
+    });
   }
   // navigating to Squidex base page
   public navigateTo() {
     browser.get(config.params.baseUrl);
-  }
-
-  public closePopUp() {
-    element(by.xpath("//span[@class='ng-tns-c3-5']")).click();
   }
 }
--- cosmos-func-tests/specs/login/login.spec.ts
+++ cosmos-func-tests/specs/login/login.spec.ts
@@ -1,5 +1,4 @@
 import { ILoginData } from "../../data/ILoginData";
-import { browser, element, ExpectedConditions, by, Browser } from "protractor";
 import { LoginPage } from "../../pages/LoginPage.po";
 import { BrowserUtil } from "../../utils/Browser.util";
 import { HomePage } from "../../pages/HomePage.po";
@@ -7,11 +6,10 @@ import { config } from "../../config";
 import constants from "../../utils/constants";
 import { CreateContent } from "../../pages/CreateContent.po";
 import SearchContent from "../../pages/SearchContent.po";
-import { AssertionError } from "assert";
 
 describe("Create Commentary", () => {
-  let loginData = require("../../../data/login.json");
-  var authors: Array<ILoginData> = loginData.authors;
+  const loginData = require("../../../data/login.json");
+  const authors: ILoginData[] = loginData.authors;
   let loginPg: LoginPage;
   let homePg: HomePage;
   let browserPg: BrowserUtil;
@@ -26,28 +24,26 @@ describe("Create Commentary", () => {
     searchContentPg = new SearchContent();
   });
 
-  // afterAll(() => {
-  //   homePg.userLogout();
-  // });
+  afterAll(() => {
+    homePg.userLogout();
+  });
 
-  it("Login with Vega Editor credentials", () => {
+  it("Login with Vega Editor credentials", async () => {
     loginPg.navigateTo();
-    loginPg.loginButton().then(() => {
-      browserPg.waitForAngularDisabledOnCurrentWindow();
-      browserPg.switchToChildWindow();
-      loginPg.login(
-        authors.find(function(obj) {
-          return obj.name === "vegaEditor";
-        })
-      );
-    });
-    browserPg.waitForAngularEnabledOnCurrentWindow().then(async () => {
-      browserPg.switchToParentWindow();
-      await expect(browserPg.getCurrentURL()).toBe(
-        config.params.expectedUrlAfterNavigation
-      );
-      await expect(homePg.userNameDisplay().getText().toString()).toBe(constants.editorWelcomeMessage);
-    });
+    loginPg.login(
+      authors.find(obj => {
+        return obj.name === "vegaEditor";
+      })
+    );
+    await expect(browserPg.getCurrentURL()).toBe(
+      config.params.expectedUrlAfterNavigation
+    );
+    await expect(
+      homePg
+        .userNameDisplay()
+        .getText()
+        .toString()
+    ).toBe(constants.editorWelcomeMessage);
   });
 
   it("should create valid commentary", async () => {
@@ -56,34 +52,33 @@ describe("Create Commentary", () => {
     createContentPg.selectCommentaryType(constants.commentaryType);
     createContentPg.selectRegion(constants.region);
     createContentPg.addCommentary(constants.contentBody);
-    const commentaryText = searchContentPg.verifyCommentaryCreation().toString();
+    const commentaryText = searchContentPg
+      .verifyCommentaryCreation()
+      .toString();
     expect(commentaryText).toBe(constants.contentBody);
   });
 
-it("Commentary should support bold text", ()=>{
- createContentPg.createCommentaryWithBoldLetters(constants.boldCommentary);
- const commentaryText = searchContentPg.verifyBoldCommentaryCreation();
- expect(commentaryText).toBe(constants.boldCommentary);
+  it("Commentary should support bold text", () => {
+    createContentPg.createCommentaryWithBoldLetters(constants.boldCommentary);
+    const commentaryText = searchContentPg.verifyBoldCommentaryCreation();
+    expect(commentaryText).toBe(constants.boldCommentary);
+  });
 
-})
+  it("Commentary should support Italic text", () => {
+    createContentPg.createCommentaryWithItalicFont(constants.italicCommentary);
+    const commentaryText = searchContentPg.verifyItalicCommentaryCreation();
+    expect(commentaryText).toBe(constants.italicCommentary);
+  });
 
-it("Commentary should support Italic text", ()=>{
-  createContentPg.createCommentaryWithItalicFont(constants.italicCommentary);
-  const commentaryText = searchContentPg.verifyItalicCommentaryCreation();
-  expect(commentaryText).toBe(constants.italicCommentary);
-})
+  it("Commentary should support numbered list", () => {
+    createContentPg.createNumberedCommentary(constants.numberedList);
+    const commentaryText = searchContentPg.verifyNumberedCommentaryCreation();
+    expect(commentaryText).toBe(constants.italicCommentary);
+  });
 
-it("Commentary should support numbered list", ()=>{
-  createContentPg.createNumberedCommentary(constants.numberedList);
-  const commentaryText = searchContentPg.verifyNumberedCommentaryCreation();
-  expect(commentaryText).toBe(constants.italicCommentary);
-
-})
-
-it("Commentary should support bulleted list", ()=>{
-  createContentPg.createBulletPointsCommentary(constants.bulletPoints);
-  const commentaryText = searchContentPg.verifyBulletPointsCommentaryCreation();
-  expect(commentaryText).toBe(constants.italicCommentary);
-})
-
+  it("Commentary should support bulleted list", () => {
+    createContentPg.createBulletPointsCommentary(constants.bulletPoints);
+    const commentaryText = searchContentPg.verifyBulletPointsCommentaryCreation();
+    expect(commentaryText).toBe(constants.italicCommentary);
+  });
 });
