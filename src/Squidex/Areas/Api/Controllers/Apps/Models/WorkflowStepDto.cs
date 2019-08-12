154a39b59ea599b11384bcd23c9cecb3d4e36a0c
Sebastian Stehle <sebastian@squidex.io>
2019-08-12 15:42:18 +0200

    Fix 500 when saving workflows.

--- src/Squidex/Areas/Api/Controllers/Apps/Models/WorkflowStepDto.cs
+++ src/Squidex/Areas/Api/Controllers/Apps/Models/WorkflowStepDto.cs
@@ -7,7 +7,9 @@
 
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Squidex.Domain.Apps.Core.Contents;
+using Squidex.Infrastructure.Reflection;
 
 namespace Squidex.Areas.Api.Controllers.Apps.Models
 {
@@ -28,5 +30,29 @@ namespace Squidex.Areas.Api.Controllers.Apps.Models
         /// Indicates if updates should not be allowed.
         /// </summary>
         public bool NoUpdate { get; set; }
+
+        public static WorkflowStepDto FromWorkflowStep(WorkflowStep step)
+        {
+            if (step == null)
+            {
+                return null;
+            }
+
+            return SimpleMapper.Map(step, new WorkflowStepDto
+            {
+                Transitions = step.Transitions.ToDictionary(
+                    y => y.Key,
+                    y => WorkflowTransitionDto.FromWorkflowTransition(y.Value))
+            });
+        }
+
+        public WorkflowStep ToStep()
+        {
+            return new WorkflowStep(
+                Transitions?.ToDictionary(
+                    y => y.Key,
+                    y => y.Value?.ToTransition()),
+                Color, NoUpdate);
+        }
     }
 }
