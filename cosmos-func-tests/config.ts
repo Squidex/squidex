ad43acf30c887782f83989f3d0433782dab432de
pavani <pavani.ramakrishna1991@gmail.com>
2019-08-22 11:59:19 +0100

    Changed http to https

--- cosmos-func-tests/config.ts
+++ cosmos-func-tests/config.ts
@@ -70,8 +70,8 @@ export let config: Config = {
             .maximize();
     },
     params: {
-        baseUrl: 'http://localhost:5000',
-        expectedUrlAfterNavigation: 'http://localhost:5000/app'
+        baseUrl: 'https://localhost:5000',
+        expectedUrlAfterNavigation: 'https://localhost:5000/app'
     },
     // protractor timeouts
     getPageTimeout: 50000,
