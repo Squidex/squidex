a5c9d841ed70134d05757fa25873787e6f5ae919
Sebastian <sebastian@squidex.io>
2019-08-27 23:26:25 +0200

    Build fix.

--- src/Squidex.Domain.Apps.Entities/Apps/Indexes/AppsByNameIndexGrain.cs
+++ src/Squidex.Domain.Apps.Entities/Apps/Indexes/AppsByNameIndexGrain.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Squidex.Infrastructure;
 using Squidex.Infrastructure.Orleans;
 using Squidex.Infrastructure.States;
 using Squidex.Infrastructure.Tasks;
