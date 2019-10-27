ac98909c16381a2cae6651d6006722ac8210c43e
Sebastian Stehle <sebastian@squidex.io>
2019-10-27 20:48:24 +0100

    Another potential drone fix.

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # Build the image
-docker build . -t squidex-build-image -f dockerfile
+docker build . -t squidex-build-image
 
 # Open the image
 docker create --name squidex-build-container squidex-build-image
 
 # Copy the output to the host file system
-docker cp squidex-build-container:/app/ ./publish
+docker cp squidex-build-container:/app/. ./publish
 
 # Cleanup
 docker rm squidex-build-container
+docker rmi squidex-build-image
