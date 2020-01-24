3889bf7d06a15ad7a5cce9c26013d7553c4efaa0
Sebastian <sebastian@squidex.io>
2020-01-24 13:52:00 +0100

    Asset test

---
+++ backend/tools/TestSuite/TestSuite.ApiTests/AssetTests.cs
@@ -0,0 +1,51 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System.IO;
+using System.Threading.Tasks;
+using TestSuite.Fixtures;
+using Xunit;
+
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+#pragma warning disable SA1507 // Code should not contain multiple blank lines in a row
+
+namespace TestSuite.ApiTests
+{
+    public class AssetTests : IClassFixture<AssetFixture>
+    {
+        public AssetFixture _ { get; }
+
+        public AssetTests(AssetFixture fixture)
+        {
+            _ = fixture;
+        }
+
+        [Fact]
+        public async Task Should_upload_image()
+        {
+            using (var stream = new FileStream("Assets/logo-squared.png", FileMode.Open))
+            {
+                var asset = await _.Assets.CreateAssetAsync("logo.png", "image/png", stream);
+
+                Assert.True(asset.IsImage);
+                Assert.Equal(600, asset.PixelHeight);
+                Assert.Equal(600, asset.PixelWidth);
+            }
+        }
+
+        [Fact]
+        public async Task Should_upload_image_without_extension()
+        {
+            using (var stream = new FileStream("Assets/logo-squared.png", FileMode.Open))
+            {
+                var asset = await _.Assets.CreateAssetAsync("logo", "image/png", stream);
+
+                Assert.False(asset.IsImage);
+            }
+        }
+    }
+}
---
+++ backend/tools/TestSuite/TestSuite.Shared/Fixtures/AssetFixture.cs
@@ -0,0 +1,21 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Squidex.ClientLibrary;
+
+namespace TestSuite.Fixtures
+{
+    public class AssetFixture : CreatedAppFixture
+    {
+        public SquidexAssetClient Assets { get; }
+
+        public AssetFixture()
+        {
+            Assets = ClientManager.GetAssetClient();
+        }
+    }
+}
