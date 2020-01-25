f7521dd2d16b20fecf3bcbc91beced49399f1cd3
Sebastian <sebastian@squidex.io>
2020-01-25 14:41:13 +0100

    Add headers

--- frontend/app/features/administration/state/users.forms.ts
+++ frontend/app/features/administration/state/users.forms.ts
@@ -1,3 +1,10 @@
+/*
+ * Squidex Headless CMS
+ *
+ * @license
+ * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
+ */
+
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Form, ValidatorsEx } from '@app/shared';
