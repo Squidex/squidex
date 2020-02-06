560d43e65ec0494a62e4388d768db9854c0ad874
Sebastian <sebastian@squidex.io>
2020-02-06 12:44:13 +0100

    Resized service.

--- frontend/app/framework/angular/sync-width.directive.ts
+++ frontend/app/framework/angular/sync-width.directive.ts
@@ -5,40 +5,46 @@
  * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
  */
 
-import { AfterViewInit, Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
-import { timer } from 'rxjs';
+import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
-import { ResourceOwner } from '@app/framework/internal';
+import {
+    ResizeListener,
+    ResizeService,
+    ResourceOwner
+} from '@app/framework/internal';
 
 @Directive({
     selector: '[sqxSyncWidth]'
 })
-export class SyncWidthDirective extends ResourceOwner implements OnInit, AfterViewInit {
+export class SyncWidthDirective extends ResourceOwner implements AfterViewInit, ResizeListener {
     @Input('sqxSyncWidth')
     public target: HTMLElement;
 
     constructor(
         private readonly element: ElementRef<HTMLElement>,
-        private readonly renderer: Renderer2
+        private readonly renderer: Renderer2,
+        private readonly resizeService: ResizeService
     ) {
         super();
-    }
 
-    public ngOnInit() {
-        this.own(timer(100, 100).subscribe(() => this.reposition()));
+        this.own(this.resizeService.listen(this.element.nativeElement, this));
     }
 
     public ngAfterViewInit() {
-        this.reposition();
+        this.onReposition();
     }
 
-    private reposition() {
-        if (!this.target) {
-            return;
+    public onResize(size: ClientRect) {
+        this.resize(size.width);
+    }
+
+    private onReposition() {
+        this.resize(this.element.nativeElement.clientWidth);
+    }
+
+    private resize(width: number) {
+        if (this.target) {
+            this.renderer.setStyle(this.target, 'width', `${width}px`);
         }
-
-        const size = this.element.nativeElement.clientWidth;
-
-        this.renderer.setStyle(this.target, 'width', `${size}px`);
     }
 }
---
+++ frontend/app/framework/services/resize.service.ts
@@ -0,0 +1,58 @@
+/*
+ * Squidex Headless CMS
+ *
+ * @license
+ * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
+ */
+
+import { Injectable, OnDestroy } from '@angular/core';
+
+import ResizeObserver from 'resize-observer-polyfill';
+
+export interface ResizeListener {
+    onResize(rect: DOMRect, element: Element): void;
+}
+
+export const ResizeServiceFactory = () => {
+    return new ResizeService();
+};
+
+@Injectable()
+export class ResizeService implements OnDestroy {
+    private readonly listeners = new WeakMap<Element, ResizeListener>();
+    private observer: ResizeObserver;
+
+    public ngOnDestroy() {
+        if (this.observer) {
+            this.observer.disconnect();
+        }
+    }
+
+    public listen(target: Element, listener: ResizeListener) {
+        if (!this.observer) {
+            this.observer = new ResizeObserver(entries => {
+                for (const entry of entries) {
+                    if (this.listeners.has(entry.target)) {
+                        const component = this.listeners.get(entry.target);
+
+                        if (component) {
+                            component.onResize(entry.contentRect as any, entry.target);
+                        }
+                    }
+                }
+            });
+        }
+
+        this.listeners.set(target, listener);
+
+        this.observer.observe(target);
+
+        return () => {
+            this.unlisten(target);
+        };
+    }
+
+    public unlisten(target: Element) {
+        this.observer.unobserve(target);
+    }
+}
