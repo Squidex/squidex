fca35cea3814b237122f9bbe275def86dfa192f7
Sebastian <sebastian@squidex.io>
2020-02-08 13:05:56 +0100

    Build fixed.

--- frontend/app-config/karma-test-shim.js
+++ frontend/app-config/karma-test-shim.js
@@ -3,33 +3,17 @@
 require('core-js/proposals/reflect-metadata');
 
 require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/proxy');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/jasmine-patch');
-require('zone.js/dist/async-test');
-require('zone.js/dist/fake-async-test');
+require('zone.js/dist/zone-testing');
 
-var testing = require('@angular/core/testing');
-var browser = require('@angular/platform-browser-dynamic/testing');
+const testing = require('@angular/core/testing');
+const browser = require('@angular/platform-browser-dynamic/testing');
 
-testing.TestBed.initTestEnvironment(
+testing.getTestBed().initTestEnvironment(
     browser.BrowserDynamicTestingModule,
     browser.platformBrowserDynamicTesting()
 );
 
-var testContext = require.context('../app', true, /\.spec\.ts/);
-
-/**
- * Get all the files, for each file, call the context function
- * that will require the file and load it up here. Context will
- * loop and require those spec files here.
- */
-function requireAll(requireContext) {
-    return requireContext.keys().map(requireContext);
-}
-
-/**
- * Requires and returns all modules that match.
- */
-var modules = requireAll(testContext);
+// Then we find all the tests.
+const context = require.context('./../app', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
