956b45decee50dc5d40e41d383ced2615e1735a6
Sebastian <sebastian@squidex.io>
2020-05-08 11:17:27 +0200

    Added test for references bug.

---
+++ backend/tools/TestSuite/TestSuite.Shared/Fixtures/ContentReferencesFixture.cs
@@ -0,0 +1,30 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System.Threading.Tasks;
+using Squidex.ClientLibrary;
+using TestSuite.Model;
+
+namespace TestSuite.Fixtures
+{
+    public sealed class ContentReferencesFixture : CreatedAppFixture
+    {
+        public string SchemaName { get; } = "references";
+
+        public IContentsClient<TestEntityWithReferences, TestEntityWithReferencesData> Contents { get; }
+
+        public ContentReferencesFixture()
+        {
+            Task.Run(async () =>
+            {
+                await TestEntityWithReferences.CreateSchemaAsync(Schemas, AppName, SchemaName);
+            }).Wait();
+
+            Contents = ClientManager.CreateContentsClient<TestEntityWithReferences, TestEntityWithReferencesData>(SchemaName);
+        }
+    }
+}
