9a915c46c50ceb8abd8f204d4f7bdc1892acfc5d
Sebastian <sebastian@squidex.io>
2020-05-08 16:33:31 +0200

    Simplify persisted grant.

--- backend/src/Squidex.Domain.Users.MongoDb/Infrastructure/MongoPersistedGrantStore.cs
+++ backend/src/Squidex.Domain.Users.MongoDb/Infrastructure/MongoPersistedGrantStore.cs
@@ -10,9 +10,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
 using IdentityServer4.Stores;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using Squidex.Infrastructure.MongoDb;
 
@@ -26,7 +24,7 @@ namespace Squidex.Domain.Users.MongoDb.Infrastructure
             {
                 cm.AutoMap();
 
-                cm.MapIdProperty(x => x.Key).SetSerializer(new StringSerializer(BsonType.ObjectId));
+                cm.MapIdProperty(x => x.Key);
             });
         }
 
