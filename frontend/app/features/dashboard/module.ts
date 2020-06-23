3b4857fb8ded77ce2568f260c81014eb1c3221cd
Sebastian <sebastian@squidex.io>
2020-06-23 15:19:17 +0200

    Seperate components.

--- frontend/app/features/dashboard/module.ts
+++ frontend/app/features/dashboard/module.ts
@@ -5,11 +5,14 @@
  * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
  */
 
+// tslint:disable: max-line-length
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SqxFrameworkModule, SqxSharedModule } from '@app/shared';
+import { GridsterModule } from 'angular-gridster2';
 import { ChartModule } from 'angular2-chartjs';
-import { DashboardPageComponent } from './declarations';
+import { ApiCallsCardComponent,  ApiCallsSummaryCardComponent, ApiCardComponent, ApiPerformanceCardComponent, ApiTrafficCardComponent, AssetUploadsCountCardComponent, AssetUploadsSizeCardComponent, AssetUploadsSizeSummaryCardComponent, DashboardPageComponent, GithubCardComponent, HistoryCardComponent, SchemaCardComponent, SupportCardComponent } from './declarations';
 
 const routes: Routes = [
     {
@@ -21,12 +24,25 @@ const routes: Routes = [
 @NgModule({
     imports: [
         ChartModule,
+        GridsterModule,
         SqxFrameworkModule,
         SqxSharedModule,
         RouterModule.forChild(routes)
     ],
     declarations: [
-        DashboardPageComponent
+        ApiCallsCardComponent,
+        ApiCallsSummaryCardComponent,
+        ApiCardComponent,
+        ApiPerformanceCardComponent,
+        ApiTrafficCardComponent,
+        AssetUploadsCountCardComponent,
+        AssetUploadsSizeCardComponent,
+        AssetUploadsSizeSummaryCardComponent,
+        DashboardPageComponent,
+        GithubCardComponent,
+        HistoryCardComponent,
+        SchemaCardComponent,
+        SupportCardComponent
     ]
 })
 export class SqxFeatureDashboardModule {}
--- frontend/app/features/dashboard/pages/dashboard-page.component.ts
+++ frontend/app/features/dashboard/pages/dashboard-page.component.ts
@@ -6,22 +6,8 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { AppsState, AuthService, DateTime, fadeAnimation, HistoryEventDto, HistoryService, LocalStoreService, ResourceOwner, UsagesService } from '@app/shared';
+import { AppsState, AuthService, CallsUsageDto, CurrentStorageDto, DateTime, fadeAnimation, LocalStoreService, ResourceOwner, StorageUsagePerDateDto, UsagesService } from '@app/shared';
 import { switchMap } from 'rxjs/operators';
-
-const COLORS: ReadonlyArray<string> = [
-    ' 51, 137, 213',
-    '211,  50,  50',
-    '131, 211,  50',
-    ' 50, 211, 131',
-    ' 50, 211, 211',
-    ' 50, 131, 211',
-    ' 50,  50, 211',
-    ' 50, 211,  50',
-    '131,  50, 211',
-    '211,  50, 211',
-    '211,  50, 131'
-];
 
 @Component({
     selector: 'sqx-dashboard-page',
@@ -32,203 +18,53 @@ const COLORS: ReadonlyArray<string> = [
     ]
 })
 export class DashboardPageComponent extends ResourceOwner implements OnInit {
-    private isStackedValue: boolean;
+    public isStacked: boolean;
 
-    public chartOptions = {
-        responsive: true,
-        scales: {
-            xAxes: [{
-                display: true,
-                stacked: false
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                stacked: false
-            }]
-        },
-        maintainAspectRatio: false
-    };
+    public storageUsage: ReadonlyArray<StorageUsagePerDateDto>;
+    public storageCurrent: CurrentStorageDto;
 
-    public stackedChartOptions = {
-        responsive: true,
-        scales: {
-            xAxes: [{
-                display: true,
-                stacked: true
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                stacked: true
-            }]
-        },
-        maintainAspectRatio: false
-    };
-
-    public history: ReadonlyArray<HistoryEventDto> = [];
-
-    public profileDisplayName = '';
-
-    public chartStorageCount: any;
-    public chartStorageSize: any;
-    public chartCallsCount: any;
-    public chartCallsBytes: any;
-    public chartCallsPerformance: any;
-
-    public storageCurrent = 0;
-    public storageAllowed = 0;
-
-    public callsPerformance = 0;
-    public callsCurrent = 0;
-    public callsAllowed = 0;
-    public callsBytes = 0;
-
-    public get isStacked() {
-        return this.isStackedValue;
-    }
-
-    public set isStacked(value: boolean) {
-        this.localStore.setBoolean('dashboard.charts.stacked', value);
-
-        this.isStackedValue = value;
-    }
+    public callsUsage: CallsUsageDto;
 
     constructor(
         public readonly appsState: AppsState,
         public readonly authState: AuthService,
-        private readonly historyService: HistoryService,
         private readonly usagesService: UsagesService,
         private readonly localStore: LocalStoreService
     ) {
         super();
 
-        this.isStackedValue = localStore.getBoolean('dashboard.charts.stacked');
+        this.isStacked = localStore.getBoolean('dashboard.charts.stacked');
     }
 
     public ngOnInit() {
-        this.own(
-            this.appsState.selectedApp.pipe(
-                    switchMap(app => this.usagesService.getTodayStorage(app.name)))
-                .subscribe(dto => {
-                    this.storageCurrent = dto.size;
-                    this.storageAllowed = dto.maxAllowed;
-                }));
-
-        this.own(
-            this.appsState.selectedApp.pipe(
-                    switchMap(app => this.historyService.getHistory(app.name, '')))
-                .subscribe(dto => {
-                    this.history = dto;
-                }));
-
         const dateTo = DateTime.today().toStringFormat('yyyy-MM-dd');
         const dateFrom = DateTime.today().addDays(-20).toStringFormat('yyyy-MM-dd');
 
         this.own(
             this.appsState.selectedApp.pipe(
+                    switchMap(app => this.usagesService.getTodayStorage(app.name)))
+                .subscribe(dto => {
+                    this.storageCurrent = dto;
+                }));
+
+        this.own(
+            this.appsState.selectedApp.pipe(
                     switchMap(app => this.usagesService.getStorageUsages(app.name, dateFrom, dateTo)))
                 .subscribe(dtos => {
-                    const labels = createLabels(dtos);
-
-                    this.chartStorageCount = {
-                        labels,
-                        datasets: [
-                            {
-                                label: 'All',
-                                lineTension: 0,
-                                fill: false,
-                                backgroundColor: `rgba(${COLORS[0]}, 0.6)`,
-                                borderColor: `rgba(${COLORS[0]}, 1)`,
-                                borderWidth: 1,
-                                data: dtos.map(x => x.totalCount)
-                            }
-                        ]
-                    };
-
-                    this.chartStorageSize = {
-                        labels,
-                        datasets: [
-                            {
-                                label: 'All',
-                                lineTension: 0,
-                                fill: false,
-                                backgroundColor: `rgba(${COLORS[0]}, 0.6)`,
-                                borderColor: `rgba(${COLORS[0]}, 1)`,
-                                borderWidth: 1,
-                                data: dtos.map(x => Math.round(100 * (x.totalSize / (1024 * 1024))) / 100)
-                            }
-                        ]
-                    };
+                    this.storageUsage = dtos;
                 }));
 
         this.own(
             this.appsState.selectedApp.pipe(
                     switchMap(app => this.usagesService.getCallsUsages(app.name, dateFrom, dateTo)))
-                .subscribe(({ details, totalBytes, totalCalls, allowedCalls, averageElapsedMs }) => {
-                    const labels = createLabelsFromSet(details);
-
-                    this.chartCallsCount = {
-                        labels,
-                        datasets: Object.keys(details).map((k, i) => (
-                            {
-                                label: label(k),
-                                backgroundColor: `rgba(${COLORS[i]}, 0.6)`,
-                                borderColor: `rgba(${COLORS[i]}, 1)`,
-                                borderWidth: 1,
-                                data: details[k].map(x => x.totalCalls)
-                            }))
-                    };
-
-                    this.chartCallsBytes = {
-                        labels,
-                        datasets: Object.keys(details).map((k, i) => (
-                            {
-                                label: label(k),
-                                backgroundColor: `rgba(${COLORS[i]}, 0.6)`,
-                                borderColor: `rgba(${COLORS[i]}, 1)`,
-                                borderWidth: 1,
-                                data: details[k].map(x => Math.round(100 * (x.totalBytes / (1024 * 1024))) / 100)
-                            }))
-                    };
-
-                    this.chartCallsPerformance = {
-                        labels,
-                        datasets: Object.keys(details).map((k, i) => (
-                            {
-                                label: label(k),
-                                backgroundColor: `rgba(${COLORS[i]}, 0.6)`,
-                                borderColor: `rgba(${COLORS[i]}, 1)`,
-                                borderWidth: 1,
-                                data: details[k].map(x => x.averageElapsedMs)
-                            }))
-                    };
-
-                    this.callsPerformance = averageElapsedMs;
-                    this.callsBytes = totalBytes;
-                    this.callsCurrent = totalCalls;
-                    this.callsAllowed = allowedCalls;
+                .subscribe(dto => {
+                    this.callsUsage = dto;
                 }));
     }
 
-    public downloadLog() {
-        this.usagesService.getLog(this.appsState.appName)
-            .subscribe(url => {
-                window.open(url, '_blank');
-            });
+    public changeIsStacked(value: boolean) {
+        this.localStore.setBoolean('dashboard.charts.stacked', value);
+
+        this.isStacked = value;
     }
-}
-
-function label(category: string) {
-    return category === '*' ? 'anonymous' : category;
-}
-
-function createLabels(dtos: ReadonlyArray<{ date: DateTime }>): ReadonlyArray<string> {
-    return dtos.map(d => d.date.toStringFormat('M-dd'));
-}
-
-function createLabelsFromSet(dtos: { [category: string]: ReadonlyArray<{ date: DateTime }> }): ReadonlyArray<string> {
-    return createLabels(dtos[Object.keys(dtos)[0]]);
 }
