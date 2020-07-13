b488cfb40bc5c9dfa3a8a72d2fb417bfc70369fa
Sebastian <sebastian@squidex.io>
2020-07-13 22:07:58 +0200

    Error simulation

--- backend/src/Squidex.Web/ApiExceptionConverter.cs
+++ backend/src/Squidex.Web/ApiExceptionConverter.cs
@@ -34,11 +34,20 @@ namespace Squidex.Web
             [500] = "https://tools.ietf.org/html/rfc7231#section-6.6.1"
         };
 
+        public static (ErrorDto Error, bool WellKnown) ToErrorDto(int statusCode, HttpContext? httpContext)
+        {
+            var error = new ErrorDto { StatusCode = statusCode };
+
+            Enrich(httpContext, error);
+
+            return (error, true);
+        }
+
         public static (ErrorDto Error, bool WellKnown) ToErrorDto(this ProblemDetails problem, HttpContext? httpContext)
         {
             Guard.NotNull(problem, nameof(problem));
 
-            var error = new ErrorDto { Message = problem.Title, StatusCode = problem.Status };
+            var error = CreateError(problem.Status ?? 500, problem.Title);
 
             Enrich(httpContext, error);
 
@@ -60,10 +69,12 @@ namespace Squidex.Web
         {
             error.TraceId = Activity.Current?.Id ?? httpContext?.TraceIdentifier;
 
-            if (error.StatusCode.HasValue)
+            if (error.StatusCode == 0)
             {
-                error.Type = Links.GetOrDefault(error.StatusCode.Value);
+                error.StatusCode = 500;
             }
+
+            error.Type = Links.GetOrDefault(error.StatusCode);
         }
 
         private static (ErrorDto Error, bool WellKnown) CreateError(Exception exception)
@@ -71,62 +82,36 @@ namespace Squidex.Web
             switch (exception)
             {
                 case ValidationException ex:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 400,
-                        Message = ex.Summary,
-                        Details = ToDetails(ex)
-                    }, true);
+                    return (CreateError(400, ex.Summary, ToDetails(ex)), true);
 
                 case DomainObjectNotFoundException _:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 404,
-                        Message = null!
-                    }, true);
+                    return (CreateError(404), true);
 
                 case DomainObjectVersionException _:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 412,
-                        Message = exception.Message
-                    }, true);
+                    return (CreateError(412, exception.Message), true);
 
                 case DomainForbiddenException _:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 403,
-                        Message = exception.Message
-                    }, true);
+                    return (CreateError(403, exception.Message), true);
 
                 case DomainException _:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 400,
-                        Message = exception.Message
-                    }, true);
+                    return (CreateError(400, exception.Message), true);
 
                 case SecurityException _:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 403,
-                        Message = "Forbidden"
-                    }, false);
+                    return (CreateError(403), false);
 
                 case DecoderFallbackException _:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 400,
-                        Message = exception.Message
-                    }, true);
+                    return (CreateError(400, exception.Message), true);
 
                 default:
-                    return (new ErrorDto
-                    {
-                        StatusCode = 500,
-                        Message = "Server Error"
-                    }, false);
+                    return (CreateError(500), false);
             }
+        }
+
+        private static ErrorDto CreateError(int status, string? message = null, string[]? details = null)
+        {
+            var error = new ErrorDto { StatusCode = status, Message = message, Details = details };
+
+            return error;
         }
 
         private static string[] ToDetails(ValidationException ex)
