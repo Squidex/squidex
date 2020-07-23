687b1190ad60e1be4337b8cbbbf4b40ce465c2d4
Sebastian <sebastian@squidex.io>
2020-07-23 10:46:50 +0200

    Texts improved.

--- backend/src/Squidex.Domain.Apps.Entities/Schemas/Indexes/SchemasIndex.cs
+++ backend/src/Squidex.Domain.Apps.Entities/Schemas/Indexes/SchemasIndex.cs
@@ -178,7 +178,7 @@ namespace Squidex.Domain.Apps.Entities.Schemas.Indexes
 
                 if (token == null)
                 {
-                    throw new ValidationException("A schema with this name already exists.");
+                    throw new ValidationException("A schema with the same name already exists.");
                 }
 
                 return token;
