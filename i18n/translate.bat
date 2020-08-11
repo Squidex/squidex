5a90a68c4f4acb2ee15408248b903191b45a28e5
Sebastian <sebastian@squidex.io>
2020-08-11 23:14:23 +0200

    Translatiopns updated.

--- i18n/translate.bat
+++ i18n/translate.bat
@@ -1,3 +1,3 @@
-cd D:\squidex-translator\Squidex.Translator\bin\Debug\netcoreapp3.1
-dotnet .\Squidex.Translator.dll translate check-frontend D:\Squidex
-dotnet .\Squidex.Translator.dll translate gen-backend D:\Squidex
+cd translator\Squidex.Translator
+dotnet run translate gen-frontend D:\Squidex
+dotnet run translate gen-backend D:\Squidex
