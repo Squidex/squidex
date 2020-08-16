0709a51aa720cde75f75ba009c06dc1ea8b63c4b
MartijnDijkgraaf <martijndijkgraaf96@gmail.com>
2020-08-16 19:53:24 +0200

    Release/4.x martijn (#555)
    * Fix for the path of the translations
    
    * Created an option to get all the translations keys
    
    * Review points solved
    
    * Removed the saveKey from the save function
    
    Co-authored-by: Martijn Dijkgraaf <martijndijkgraaf@MacBook-Pro-van-Martijn.local>

---
+++ backend/i18n/translator/Squidex.Translator/Processes/GenerateKeys.cs
@@ -0,0 +1,39 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+using System.IO;
+using System.Resources.NetStandard;
+using Squidex.Translator.State;
+
+namespace Squidex.Translator.Processes
+{
+    public sealed class GenerateKeys
+    {
+        private readonly TranslationService service;
+        private readonly DirectoryInfo folder;
+
+        public GenerateKeys(DirectoryInfo folder, TranslationService service, string type)
+        {
+            if (type == "frontend")
+            {
+                this.folder = Frontend.GetFolder(folder);
+            }
+            else if (type == "backend")
+            {
+                this.folder = new DirectoryInfo(Path.Combine(folder.FullName, "backend", "src", "Squidex.Shared"));
+            }
+
+            this.service = service;
+        }
+
+        public void Run()
+        {
+            service.SaveKeys();
+        }
+    }
+}
--- backend/i18n/translator/Squidex.Translator/State/TranslationService.cs
+++ backend/i18n/translator/Squidex.Translator/State/TranslationService.cs
@@ -76,7 +76,7 @@ namespace Squidex.Translator.State
 
         private void Save<T>(string name, T value) where T : new()
         {
-            var path = directory.FullName + name;
+            var path = Path.Combine(directory.FullName, this.fileName + name);
 
             WriteTo(value, path);
         }
@@ -124,6 +124,18 @@ namespace Squidex.Translator.State
 
             Save("__todos.json", translationsTodo);
             Save("__ignore.json", translationToIgnore);
+        }
+
+        public void SaveKeys()
+        {
+            var translationKeys = new TranslationKeys();
+
+            foreach (var text in Texts)
+            {
+                translationKeys.Add(text.Key, string.Empty);
+            }
+
+            Save("__keys.json", translationKeys);
         }
 
         public void Translate(string fileName, string text, string originText, Action<string> handler, bool silent = false)
