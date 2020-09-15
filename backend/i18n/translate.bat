45e2537f8def710a869f2441a19d83010b852165
fpisu-sardit <70273645+fpisu-sardit@users.noreply.github.com>
2020-09-15 18:31:04 +0200

    add italian translation (#574)
    * add italian translation

--- backend/i18n/translate.bat
+++ backend/i18n/translate.bat
@@ -1,7 +1,8 @@
 cd translator\Squidex.Translator
 
-dotnet run translate check-backend D:\Squidex
-dotnet run translate check-frontend D:\Squidex
+dotnet run translate check-backend C:\Users\federica.pisu\squidex\mysquidex\squidex
+dotnet run translate check-frontend C:\Users\federica.pisu\squidex\mysquidex\squidex
 
-dotnet run translate gen-frontend D:\Squidex
-dotnet run translate gen-backend D:\Squidex
+dotnet run translate gen-frontend C:\Users\federica.pisu\squidex\mysquidex\squidex
+dotnet run translate gen-backend C:\Users\federica.pisu\squidex\mysquidex\squidex
+
