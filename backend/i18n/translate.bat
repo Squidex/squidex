3c1ba70912bff813206ad7ac74dda482b50263ec
Federica Pisu <fpisu@sardegnait.it>
2020-09-15 16:53:24 +0200

    add italian translation

--- backend/i18n/translate.bat
+++ backend/i18n/translate.bat
@@ -1,7 +1,8 @@
 cd translator\Squidex.Translator
 
-dotnet run translate check-backend C:\Users\federica.pisu\squidex\squidex
-dotnet run translate check-frontend C:\Users\federica.pisu\squidex\squidex
+dotnet run translate check-backend C:\Users\federica.pisu\squidex\mysquidex\squidex
+dotnet run translate check-frontend C:\Users\federica.pisu\squidex\mysquidex\squidex
 
-dotnet run translate gen-frontend C:\Users\federica.pisu\squidex\squidex
-dotnet run translate gen-backend C:\Users\federica.pisu\squidex\squidex
+dotnet run translate gen-frontend C:\Users\federica.pisu\squidex\mysquidex\squidex
+dotnet run translate gen-backend C:\Users\federica.pisu\squidex\mysquidex\squidex
+
