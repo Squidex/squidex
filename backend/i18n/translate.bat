41c395d533ebd5b88e14579345731a5f16efd4a0
Sebastian <sebastian@squidex.io>
2020-09-15 19:35:42 +0200

    Code cleanup.

--- backend/i18n/translate.bat
+++ backend/i18n/translate.bat
@@ -1,8 +1,8 @@
 cd translator\Squidex.Translator
 
-dotnet run translate check-backend C:\Users\federica.pisu\squidex\mysquidex\squidex
-dotnet run translate check-frontend C:\Users\federica.pisu\squidex\mysquidex\squidex
+dotnet run translate check-backend ..\..\..\..
+dotnet run translate check-frontend ..\..\..\..
 
-dotnet run translate gen-frontend C:\Users\federica.pisu\squidex\mysquidex\squidex
-dotnet run translate gen-backend C:\Users\federica.pisu\squidex\mysquidex\squidex
+dotnet run translate gen-frontend ..\..\..\..
+dotnet run translate gen-backend ..\..\..\..
 
