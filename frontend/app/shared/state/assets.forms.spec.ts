e9bd69b81bcbc9b3c20d2f11cf39e746a25d776b
Sebastian <sebastian@squidex.io>
2020-10-02 20:02:36 +0200

    Something

--- frontend/app/shared/state/assets.forms.spec.ts
+++ frontend/app/shared/state/assets.forms.spec.ts
@@ -41,6 +41,7 @@ describe('AnnotateAssetForm', () => {
 
     it('should create slug from file name', () => {
         form.load(asset);
+
         form.generateSlug({} as any);
 
         const slug = form.form.get('slug')!.value;
@@ -50,6 +51,7 @@ describe('AnnotateAssetForm', () => {
 
     it('should create slug from file name and append extension', () => {
         form.form.get('fileName')!.setValue('My New File');
+
         form.generateSlug(asset);
 
         const slug = form.form.get('slug')!.value;
