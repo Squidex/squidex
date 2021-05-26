39e7398d19fe2a52b3b3c6ed5880098ddeb99494
Sebastian Stehle <sebastian@squidex.io>
2021-05-26 10:47:12 +0200

    FIx confirm click.

--- frontend/app/framework/angular/forms/confirm-click.directive.ts
+++ frontend/app/framework/angular/forms/confirm-click.directive.ts
@@ -44,9 +44,10 @@ export class ConfirmClickDirective {
             this.confirmTitle.length > 0 &&
             this.confirmText &&
             this.confirmText.length > 0) {
-            const destinations = this.clickConfirmed.observers?.map(x => (x as Subscriber<any>)['destination']) || [];
-
             this.beforeClick.emit();
+
+            const destinations =
+                this.clickConfirmed.observers?.map(x => (x as Subscriber<any>)['destination']) || [];
 
             this.dialogs.confirm(this.confirmTitle, this.confirmText, this.confirmRememberKey).pipe(take(1))
                 .subscribe(confirmed => {
