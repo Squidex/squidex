12d292a05443406ec6488d89c74f48d06e94cd4d
Sebastian Stehle <sebastian@squidex.io>
2021-09-01 17:41:44 +0200

    Special content id for API. (#753)

---
+++ backend/src/Squidex.Web/CommandMiddlewares/EnrichWithContentIdCommandMiddleware.cs
@@ -0,0 +1,31 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System.Threading.Tasks;
+using Squidex.Domain.Apps.Entities.Contents.Commands;
+using Squidex.Infrastructure.Commands;
+
+namespace Squidex.Web.CommandMiddlewares
+{
+    public sealed class EnrichWithContentIdCommandMiddleware : ICommandMiddleware
+    {
+        private const string SingletonId = "_schemaId_";
+
+        public Task HandleAsync(CommandContext context, NextDelegate next)
+        {
+            if (context.Command is ContentCommand contentCommand && contentCommand is not CreateContent)
+            {
+                if (contentCommand.ContentId.ToString().Equals(SingletonId))
+                {
+                    contentCommand.ContentId = contentCommand.SchemaId.Id;
+                }
+            }
+
+            return next(context);
+        }
+    }
+}
