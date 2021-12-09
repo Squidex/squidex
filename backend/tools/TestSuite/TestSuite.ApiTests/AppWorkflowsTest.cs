52ad17c2e650a73765eb18c384c88581eecb0b9f
Sebastian <sebastian@squidex.io>
2021-12-09 18:45:26 +0100

    Refactor tests into smaller chunks.

---
+++ backend/tools/TestSuite/TestSuite.ApiTests/AppWorkflowsTest.cs
@@ -0,0 +1,99 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Squidex.ClientLibrary.Management;
+using TestSuite.Fixtures;
+using Xunit;
+
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+#pragma warning disable SA1507 // Code should not contain multiple blank lines in a row
+
+namespace TestSuite.ApiTests
+{
+    public sealed class AppWorkflowsTests : IClassFixture<CreatedAppFixture>
+    {
+        private readonly string name = Guid.NewGuid().ToString();
+
+        public CreatedAppFixture _ { get; }
+
+        public AppWorkflowsTests(CreatedAppFixture fixture)
+        {
+            _ = fixture;
+        }
+
+        [Fact]
+        public async Task Should_create_workflow()
+        {
+            // STEP 0: Create workflow.
+            var workflow = await CreateAsync();
+
+            Assert.NotNull(workflow);
+            Assert.NotNull(workflow.Name);
+            Assert.Equal(3, workflow.Steps.Count);
+        }
+
+        [Fact]
+        public async Task Should_update_workflow()
+        {
+            // STEP 0: Create workflow.
+            var workflow = await CreateAsync();
+
+
+            // STEP 1: Update workflow.
+            var updateRequest = new UpdateWorkflowDto
+            {
+                Initial = "Draft",
+                Steps = new Dictionary<string, WorkflowStepDto>
+                {
+                    ["Draft"] = new WorkflowStepDto
+                    {
+                        Transitions = new Dictionary<string, WorkflowTransitionDto>
+                        {
+                            ["Published"] = new WorkflowTransitionDto()
+                        }
+                    },
+                    ["Published"] = new WorkflowStepDto(),
+                },
+                Name = name
+            };
+
+            var workflows_2 = await _.Apps.PutWorkflowAsync(_.AppName, workflow.Id, updateRequest);
+            var workflow_2 = workflows_2.Items.Find(x => x.Name == name);
+
+            Assert.NotNull(workflow_2);
+            Assert.NotNull(workflow_2.Name);
+            Assert.Equal(2, workflow_2.Steps.Count);
+        }
+
+        [Fact]
+        public async Task Should_delete_workflow()
+        {
+            // STEP 0: Create workflow.
+            var workflow = await CreateAsync();
+
+
+            // STEP 1: Delete workflow.
+            var workflows_2 = await _.Apps.DeleteWorkflowAsync(_.AppName, workflow.Id);
+
+            // Should not return deleted workflow.
+            Assert.DoesNotContain(workflows_2.Items, x => x.Name == name);
+        }
+
+        private async Task<WorkflowDto> CreateAsync()
+        {
+            var createRequest = new AddWorkflowDto
+            {
+                Name = name
+            };
+
+            var workflows = await _.Apps.PostWorkflowAsync(_.AppName, createRequest);
+            var workflow = workflows.Items.Find(x => x.Name == name);
+
+            return workflow;
+        }
+    }
+}
