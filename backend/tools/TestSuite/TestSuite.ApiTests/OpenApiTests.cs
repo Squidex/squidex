e733ec849fb3d6cc153b490dc15b62678a756a1f
Sebastian <sebastian@squidex.io>
2022-02-01 15:48:01 +0100

    More OpenAPI tests

--- backend/tools/TestSuite/TestSuite.ApiTests/OpenApiTests.cs
+++ backend/tools/TestSuite/TestSuite.ApiTests/OpenApiTests.cs
@@ -13,19 +13,39 @@ using Xunit;
 
 namespace TestSuite.ApiTests
 {
-    public class OpenApiTests : IClassFixture<ClientManagerFixture>
+    public class OpenApiTests : IClassFixture<ContentFixture>
     {
-        public ClientManagerFixture _ { get; }
+        public ContentFixture _ { get; }
 
-        public OpenApiTests(ClientManagerFixture fixture)
+        public OpenApiTests(ContentFixture fixture)
         {
             _ = fixture;
         }
 
         [Fact]
-        public async Task Should_provide_spec()
+        public async Task Should_provide_general_spec()
         {
             var url = $"{_.ClientManager.Options.Url}/api/swagger/v1/swagger.json";
+
+            var document = await OpenApiDocument.FromUrlAsync(url);
+
+            Assert.NotNull(document);
+        }
+
+        [Fact]
+        public async Task Should_provide_content_spec()
+        {
+            var url = $"{_.ClientManager.Options.Url}/api/content/{_.AppName}/swagger/v1/swagger.json";
+
+            var document = await OpenApiDocument.FromUrlAsync(url);
+
+            Assert.NotNull(document);
+        }
+
+        [Fact]
+        public async Task Should_provide_flat_content_spec()
+        {
+            var url = $"{_.ClientManager.Options.Url}/api/content/{_.AppName}/flat/swagger/v1/swagger.json";
 
             var document = await OpenApiDocument.FromUrlAsync(url);
 
