9d8fe84e233cb21319f0cc641fedf8b54e8377c1
Sebastian Stehle <sebastian@squidex.io>
2022-04-08 19:06:38 +0200

    Templates test (#865)
    * Test the templates.
    
    * Check docker URL.
    
    * Test
    
    * Fix identity.

--- backend/src/Squidex.Domain.Apps.Entities/Apps/Templates/TemplatesOptions.cs
+++ backend/src/Squidex.Domain.Apps.Entities/Apps/Templates/TemplatesOptions.cs
@@ -5,12 +5,12 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
-#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-
 namespace Squidex.Domain.Apps.Entities.Apps.Templates
 {
     public sealed class TemplatesOptions
     {
+        public string? LocalUrl { get; set; }
+
         public TemplateRepository[] Repositories { get; set; }
     }
 }
