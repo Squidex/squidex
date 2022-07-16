21a981aff5fc5ed0c09c540af416c9342f8cad93
Sebastian <sebastian@squidex.io>
2022-07-16 16:29:39 +0200

    Add missing files.

---
+++ backend/tools/TestSuite/TestSuite.ApiTests/CommentsTests.cs
@@ -0,0 +1,91 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Squidex.ClientLibrary.Management;
+using TestSuite.Fixtures;
+using Xunit;
+
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+#pragma warning disable SA1507 // Code should not contain multiple blank lines in a row
+
+namespace TestSuite.ApiTests
+{
+    public class CommentsTests : IClassFixture<CreatedAppFixture>
+    {
+        private readonly string resource = Guid.NewGuid().ToString();
+
+        public CreatedAppFixture _ { get; }
+
+        public CommentsTests(CreatedAppFixture fixture)
+        {
+            _ = fixture;
+        }
+
+        [Fact]
+        public async Task Should_make_watch_request()
+        {
+            var result = await _.Comments.GetWatchingUsersAsync(_.AppName, resource);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Should_create_comment()
+        {
+            // STEP 1: Create the comment.
+            var createRequest = new UpsertCommentDto { Text = resource };
+
+            await _.Comments.PostCommentAsync(_.AppName, resource, createRequest);
+
+
+            // STEP 2: Get comments
+            var comments = await _.Comments.GetCommentsAsync(_.AppName, resource);
+
+            Assert.Contains(comments.CreatedComments, x => x.Text == createRequest.Text);
+        }
+
+        [Fact]
+        public async Task Should_update_comment()
+        {
+            // STEP 1: Create the comment.
+            var createRequest = new UpsertCommentDto { Text = resource };
+
+            var comment = await _.Comments.PostCommentAsync(_.AppName, resource, createRequest);
+
+
+            // STEP 2: Update comment.
+            var updateRequest = new UpsertCommentDto { Text = $"{resource}_Update" };
+
+            await _.Comments.PutCommentAsync(_.AppName, resource, comment.Id, updateRequest);
+
+
+            // STEP 3: Get comments since create.
+            var comments = await _.Comments.GetCommentsAsync(_.AppName, resource, 0);
+
+            Assert.Contains(comments.UpdatedComments, x => x.Text == updateRequest.Text);
+        }
+
+        [Fact]
+        public async Task Should_delete_comment()
+        {
+            // STEP 1: Create the comment.
+            var createRequest = new UpsertCommentDto { Text = resource };
+
+            var comment = await _.Comments.PostCommentAsync(_.AppName, resource, createRequest);
+
+
+            // STEP 2: Delete comment.
+            await _.Comments.DeleteCommentAsync(_.AppName, resource, comment.Id);
+
+
+            // STEP 3: Get comments since create.
+            var comments = await _.Comments.GetCommentsAsync(_.AppName, resource, 0);
+
+            Assert.Contains(comment.Id, comments.DeletedComments);
+        }
+    }
+}
---
+++ backend/tools/TestSuite/TestSuite.Shared/ClientExtensions.cs
@@ -0,0 +1,65 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Squidex.ClientLibrary.Management;
+
+namespace TestSuite
+{
+    public static class ClientExtensions
+    {
+        public static async Task<BackupJobDto> WaitForBackupAsync(this IBackupsClient backupsClient, string app, TimeSpan timeout)
+        {
+            try
+            {
+                using var cts = new CancellationTokenSource(timeout);
+
+                while (!cts.IsCancellationRequested)
+                {
+                    var backups = await backupsClient.GetBackupsAsync(app, cts.Token);
+                    var backup = backups.Items.Find(x => x.Status == JobStatus.Completed || x.Status == JobStatus.Failed);
+
+                    if (backup != null)
+                    {
+                        return backup;
+                    }
+
+                    await Task.Delay(200, cts.Token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            return null;
+        }
+
+        public static async Task<RestoreJobDto> WaitForRestoreAsync(this IBackupsClient backupsClient, Uri url, TimeSpan timeout)
+        {
+            try
+            {
+                using var cts = new CancellationTokenSource(timeout);
+
+                while (!cts.IsCancellationRequested)
+                {
+                    var restore = await backupsClient.GetRestoreJobAsync(cts.Token);
+
+                    if (restore.Url == url && restore.Status is JobStatus.Completed or JobStatus.Failed)
+                    {
+                        return restore;
+                    }
+
+                    await Task.Delay(200, cts.Token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            return null;
+        }
+    }
+}
