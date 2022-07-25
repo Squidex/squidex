78d972e6fdadf70684f832b99ab852ed89b72557
Sebastian <sebastian@squidex.io>
2022-07-25 11:23:25 +0200

    Added missing file.

---
+++ backend/src/Squidex.Infrastructure.MongoDb/MongoDb/BsonDefaultConventions.cs
@@ -0,0 +1,40 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace Squidex.Infrastructure.MongoDb
+{
+    public static class BsonDefaultConventions
+    {
+        private static bool isRegistered;
+
+        public static void Register()
+        {
+            try
+            {
+
+                if (isRegistered)
+                {
+                    return;
+                }
+
+                ConventionRegistry.Register("IgnoreExtraElements", new ConventionPack
+                {
+                    new IgnoreExtraElementsConvention(true)
+                }, t => true);
+
+                isRegistered = true;
+            }
+            catch (BsonSerializationException)
+            {
+                return;
+            }
+        }
+    }
+}
