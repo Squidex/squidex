e8a4f8612941f0cb5283796e4daef6e1d9f50eff
Sebastian <sebastian@squidex.io>
2022-07-28 19:47:47 +0200

    Revert batch change.

--- backend/src/Squidex.Domain.Apps.Entities/Assets/AssetUsageTracker_EventHandling.cs
+++ backend/src/Squidex.Domain.Apps.Entities/Assets/AssetUsageTracker_EventHandling.cs
@@ -24,7 +24,7 @@ namespace Squidex.Domain.Apps.Entities.Assets
 
         public int BatchSize
         {
-            get => 1;
+            get => 1000;
         }
 
         public int BatchDelay
