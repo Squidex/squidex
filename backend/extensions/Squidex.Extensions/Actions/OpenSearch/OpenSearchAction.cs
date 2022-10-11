2e3548ec539db95b42e32b578773c57f0dc33699
Sebastian <sebastian@squidex.io>
2022-10-11 17:43:27 +0200

    Open Search action.

--- backend/extensions/Squidex.Extensions/Actions/OpenSearch/OpenSearchAction.cs
+++ backend/extensions/Squidex.Extensions/Actions/OpenSearch/OpenSearchAction.cs
@@ -10,16 +10,16 @@ using Squidex.Domain.Apps.Core.HandleRules;
 using Squidex.Domain.Apps.Core.Rules;
 using Squidex.Infrastructure.Validation;
 
-namespace Squidex.Extensions.Actions.ElasticSearch
+namespace Squidex.Extensions.Actions.OpenSearch
 {
     [RuleAction(
-        Title = "ElasticSearch",
-        IconImage = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 29 28'><path d='M13.427 17.436H4.163C3.827 16.354 3.636 15.2 3.636 14s.182-2.355.527-3.436h15.245c1.891 0 3.418 1.545 3.418 3.445a3.421 3.421 0 0 1-3.418 3.427h-5.982zm-.436 1.146H4.6a11.508 11.508 0 0 0 4.2 4.982 11.443 11.443 0 0 0 15.827-3.209 5.793 5.793 0 0 0-4.173-1.773H12.99zm7.464-9.164a5.794 5.794 0 0 0 4.173-1.773 11.45 11.45 0 0 0-9.536-5.1c-2.327 0-4.491.7-6.3 1.891a11.554 11.554 0 0 0-4.2 4.982h15.864z'/></svg>",
-        IconColor = "#1e5470",
-        Display = "Populate ElasticSearch index",
-        Description = "Populate a full text search index in ElasticSearch.",
-        ReadMore = "https://www.elastic.co/")]
-    public sealed record ElasticSearchAction : RuleAction
+        Title = "OpenSearch",
+        IconImage = "<svg viewBox='0 0 64 64' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M61.737 23.5a2.263 2.263 0 0 0-2.262 2.263c0 18.618-15.094 33.712-33.712 33.712a2.263 2.263 0 1 0 0 4.525C46.88 64 64 46.88 64 25.763a2.263 2.263 0 0 0-2.263-2.263Z' fill='#fff'/><path d='M48.081 38c2.176-3.55 4.28-8.282 3.866-14.908C51.09 9.367 38.66-1.045 26.921.084c-4.596.441-9.314 4.187-8.895 10.896.182 2.916 1.61 4.637 3.928 5.96 2.208 1.26 5.044 2.057 8.259 2.961 3.883 1.092 8.388 2.32 11.85 4.87 4.15 3.058 6.986 6.603 6.018 13.229Z' fill='#fff'/><path d='M3.919 14C1.743 17.55-.361 22.282.052 28.908.91 42.633 13.342 53.045 25.08 51.916c4.596-.441 9.314-4.187 8.895-10.896-.182-2.916-1.61-4.637-3.928-5.96-2.208-1.26-5.044-2.057-8.259-2.961-3.883-1.092-8.388-2.32-11.85-4.87C5.787 24.17 2.95 20.625 3.919 14Z' fill='#fff'/></svg>",
+        IconColor = "#005EB8",
+        Display = "Populate OpenSearch index",
+        Description = "Populate a full text search index in OpenSearch.",
+        ReadMore = "https://opensearch.org/")]
+    public sealed record OpenSearchAction : RuleAction
     {
         [AbsoluteUrl]
         [LocalizedRequired]
