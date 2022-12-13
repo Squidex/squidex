af0c8aaddf0a5da63010caa56a87a2efffd52179
Sebastian <sebastian@squidex.io>
2022-12-13 13:26:47 +0100

    Remove old type declarations.

--- frontend/src/app/declarations.d.ts
+++ frontend/src/app/declarations.d.ts
@@ -5,20 +5,5 @@
  * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
  */
 
-declare module 'graphiql';
 declare module 'pikaday/pikaday';
 declare module 'progressbar.js';
-
-declare module 'sortablejs' {
-    export class Ref {
-        public destroy(): any;
-
-        public option(property: string, value: any): any;
-    }
-
-    export function create(element: any, options: any): Ref;
-}
-
-declare namespace marked {
-    export function escape(input: string): string;
-}
