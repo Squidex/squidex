9a6b77074292f14216b1d43d639f0ff992c8eccc
Sebastian <sebastian@squidex.io>
2022-12-19 21:48:04 +0100

    Add missing files.

---
+++ frontend/src/app/shell/pages/internal/feedback-menu.component.ts
@@ -0,0 +1,44 @@
+/*
+ * Squidex Headless CMS
+ *
+ * @license
+ * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
+ */
+
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import markerSDK, { MarkerSdk } from '@marker.io/browser';
+import { UIOptions } from '@app/shared';
+
+@Component({
+    selector: 'sqx-feedback-menu',
+    styleUrls: ['./feedback-menu.component.scss'],
+    templateUrl: './feedback-menu.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FeedbackMenuComponent implements OnInit, OnDestroy {
+    private widget?: MarkerSdk;
+
+    public markerProject = '';
+
+    constructor(uiOptions: UIOptions,
+    ) {
+        this.markerProject = uiOptions.get('markerProject');
+    }
+
+    public ngOnDestroy() {
+        this.widget?.unload();
+    }
+
+    public async ngOnInit() {
+        if (!this.markerProject) {
+            return;
+        }
+
+        this.widget = await markerSDK.loadWidget({ project: this.markerProject });
+        this.widget.hide();
+    }
+
+    public capture() {
+        this.widget?.capture('fullscreen');
+    }
+}
