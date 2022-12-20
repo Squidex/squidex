6a6b0841bc1f3232bf5f9585b44985d8d0f17d75
Sebastian <sebastian@squidex.io>
2022-12-20 20:01:56 +0100

    Fix compile error for ng dev

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+declare var require: any;
 
 export const environment = {
     production: false,
