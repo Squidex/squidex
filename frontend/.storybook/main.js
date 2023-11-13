d6066434d89bcdb5e2cc28bf6c695ad88fcf8801
Sebastian <sebastian@squidex.io>
2023-11-13 14:19:52 +0100

    Temp

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,17 +5,16 @@
  * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
  */
 
-const customConfig = require('./../src/config/webpack.config');
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
   framework: {
     name: "@storybook/angular",
     options: {}
-  },
-  webpackFinal: async config => {
-    customConfig(config, {}, {});
-    return config;
   },
   docs: {
     autodocs: true
