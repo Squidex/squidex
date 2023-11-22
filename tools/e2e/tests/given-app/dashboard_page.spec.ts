5414f2671286559c1d4b98fa06c498306a283542
Sebastian <sebastian@squidex.io>
2023-11-22 22:24:53 +0100

    Fix tests again?

--- tools/e2e/tests/given-app/dashboard_page.spec.ts
+++ tools/e2e/tests/given-app/dashboard_page.spec.ts
@@ -3,5 +3,7 @@ import { expect, test } from './_fixture';
 test('visual test', async ({ page, appName }) => {
     await page.goto(`/app/${appName}`);
 
-    await expect(page).toHaveScreenshot();
+    await page.waitForLoadState('networkidle');
+
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
 });
