b7442362cc886b2a2fb4fdcf1dd4de7d062ad88c
Sebastian <sebastian@squidex.io>
2023-11-22 22:53:04 +0100

    Make screenshot.

--- tools/e2e/tests/login.spec.ts
+++ tools/e2e/tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('login', async ({ page }) => {
+test('login', async ({ page }, testInfo) => {
+    try {
     await page.goto('/');
 
     // Start waiting for popup before clicking.
@@ -20,6 +21,12 @@ test('login', async ({ page }) => {
     await page.waitForURL(/app/);
 
     await expect(page).toHaveTitle(/Apps/);
+    } finally {
+        await testInfo.attach('login.png', {
+            body: await page.screenshot(),
+            contentType: 'image/png',
+        });
+    }
 });
 
 test('visual test', async ({ page }) => {
