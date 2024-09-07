fa5f097659a3f19e98e889e5ef5d9a850a87686f
Sebastian Stehle <sebastian@squidex.io>
2024-09-07 22:33:29 +0200

    Daily test #1

--- tools/e2e/tests/given-app/assets.spec.ts
+++ tools/e2e/tests/given-app/assets.spec.ts
@@ -70,6 +70,93 @@ test('should edit asset metadata', async ({ assetsPage }) => {
     expect(newCard.root).toBeVisible();
 });
 
+test('should add asset folder', async ({ assetsPage }) => {
+    const folderName = `folder-${getRandomId()}`;
+
+    const folderDialog = await assetsPage.openAssetFolderDialog();
+    await folderDialog.enterName(folderName);
+    await folderDialog.save();
+
+    const folderCard = await assetsPage.getAssetFolderCard(folderName);
+
+    expect(folderCard.root).toBeVisible();
+});
+
+test('should open asset folder', async ({ assetsPage }) => {
+    const folderName = `folder-${getRandomId()}`;
+
+    const folderDialog = await assetsPage.openAssetFolderDialog();
+    await folderDialog.enterName(folderName);
+    await folderDialog.save();
+
+    const folderCard = await assetsPage.getAssetFolderCard(folderName);
+    await folderCard.open();
+
+    const moveUpCard = await assetsPage.getAssetFolderCard('<Parent>');
+
+    expect(moveUpCard.root).toBeVisible();
+});
+
+test('should rename asset folder', async ({ assetsPage }) => {
+    const folderName = `folder-${getRandomId()}`;
+
+    const folderDialog = await assetsPage.openAssetFolderDialog();
+    await folderDialog.enterName(folderName);
+    await folderDialog.save();
+    const folderCard = await assetsPage.getAssetFolderCard(folderName);
+
+    const newName = `folder-${getRandomId()}`;
+
+    const renameDialog = await folderCard.rename();
+    await renameDialog.enterName(newName);
+    await renameDialog.rename();
+    const renamedCard = await assetsPage.getAssetFolderCard(newName);
+
+    expect(renamedCard.root).toBeVisible();
+});
+
+test('should delete asset folder', async ({ assetsPage }) => {
+    const folderName = `folder-${getRandomId()}`;
+
+    const folderDialog = await assetsPage.openAssetFolderDialog();
+    await folderDialog.enterName(folderName);
+    await folderDialog.save();
+
+    const folderCard = await assetsPage.getAssetFolderCard(folderName);
+
+    const dropdown = await folderCard.openOptionsDropdown();
+    await dropdown.delete();
+
+    expect(folderCard.root).not.toBeVisible();
+});
+
+test('should add asset folder to parent', async ({ assetsPage }) => {
+    const parentName = `folder-${getRandomId()}`;
+
+    const parentDialog = await assetsPage.openAssetFolderDialog();
+    await parentDialog.enterName(parentName);
+    await parentDialog.save();
+
+    const parentcard = await assetsPage.getAssetFolderCard(parentName);
+    await parentcard.open();
+
+    const childName = `folder-${getRandomId()}`;
+    const childDialog = await assetsPage.openAssetFolderDialog();
+    await childDialog.enterName(childName);
+    await childDialog.save();
+
+    const childCard = await assetsPage.getAssetFolderCard(childName);
+
+    const moveUpCard = await assetsPage.getAssetFolderCard('<Parent>');
+    await moveUpCard.open();
+
+    expect(childCard.root).not.toBeVisible();
+
+    await parentcard.open();
+
+    expect(childCard.root).toBeVisible();
+});
+
 async function uploadRandomAsset(assetsPage: AssetsPage) {
     const fileName = `file-${getRandomId()}`;
     const fileBuffer = await readFile(path.join(__dirname, '../../assets/logo-squared.png'));
