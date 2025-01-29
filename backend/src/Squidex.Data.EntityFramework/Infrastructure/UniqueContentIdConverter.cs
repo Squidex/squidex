df041bfa898c187b48b9959fdd9a2e161b7db6b0
Sebastian Stehle <sebastian@squidex.io>
2025-01-29 23:14:29 +0100

    Add missing files.

---
+++ backend/src/Squidex.Data.EntityFramework/Infrastructure/UniqueContentIdConverter.cs
@@ -0,0 +1,31 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Squidex.Domain.Apps.Entities.Contents.Text;
+
+namespace Squidex.Infrastructure;
+
+public static class UniqueContentIdConverter
+{
+    public static string ToParseableString(this UniqueContentId source)
+    {
+        return $"{source.AppId}__{source.ContentId}";
+    }
+
+    public static UniqueContentId ToUniqueContentId(this string source)
+    {
+        var separator = source.IndexOf("__", StringComparison.Ordinal);
+        if (separator < 0)
+        {
+            throw new ArgumentException("Invalid ID", nameof(source));
+        }
+
+        return new UniqueContentId(
+            DomainId.Create(source[..separator]),
+            DomainId.Create(source[(separator + 2)..]));
+    }
+}
---
+++ backend/tests/Squidex.Data.Tests/EntityFramework/Domain/Contents/Text/EFTextIndexerStateTests.cs
@@ -0,0 +1,24 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Squidex.Domain.Apps.Entities.Contents.Repositories;
+using Squidex.Domain.Apps.Entities.Contents.Text;
+using Squidex.Domain.Apps.Entities.Contents.Text.State;
+using Squidex.EntityFramework.TestHelpers;
+using Squidex.Shared;
+
+namespace Squidex.EntityFramework.Domain.Contents.Text;
+
+public sealed class EFTextIndexerStateTests(PostgresFixture fixture) : TextIndexerStateTests, IClassFixture<PostgresFixture>
+{
+    protected override Task<ITextIndexerState> CreateSutAsync(IContentRepository contentRepository)
+    {
+        var sut = new EFTextIndexerState<TestDbContext>(fixture.DbContextFactory, fixture.Dialect, contentRepository);
+
+        return Task.FromResult<ITextIndexerState>(sut);
+    }
+}
