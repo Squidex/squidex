65fb21909738d930d1975b71aee284d0bfb3f251
Sebastian Stehle <sebastian@squidex.io>
2025-04-07 23:28:18 +0200

    Add compression.

--- backend/src/Squidex/Config/Web/WebExtensions.cs
+++ backend/src/Squidex/Config/Web/WebExtensions.cs
@@ -7,9 +7,11 @@
 
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using Squidex.Infrastructure.Json;
 using Squidex.Pipeline.Robots;
+using Squidex.Web;
 using Squidex.Web.Pipeline;
 
 namespace Squidex.Config.Web;
@@ -19,14 +21,12 @@ public static class WebExtensions
     public static IApplicationBuilder UseSquidexCacheKeys(this IApplicationBuilder app)
     {
         app.UseMiddleware<CachingKeysMiddleware>();
-
         return app;
     }
 
     public static IApplicationBuilder UseSquidexLocalCache(this IApplicationBuilder app)
     {
         app.UseMiddleware<LocalCacheMiddleware>();
-
         return app;
     }
 
@@ -40,28 +40,30 @@ public static class WebExtensions
             .AddSupportedUICultures(supportedCultures);
 
         app.UseRequestLocalization(localizationOptions);
-
         return app;
     }
 
     public static IApplicationBuilder UseSquidexLogging(this IApplicationBuilder app)
     {
         app.UseMiddleware<RequestLogPerformanceMiddleware>();
-
         return app;
     }
 
     public static IApplicationBuilder UseSquidexUsage(this IApplicationBuilder app)
     {
         app.UseMiddleware<UsageMiddleware>();
-
         return app;
     }
 
     public static IApplicationBuilder UseSquidexExceptionHandling(this IApplicationBuilder app)
     {
         app.UseMiddleware<RequestExceptionMiddleware>();
+        return app;
+    }
 
+    public static IApplicationBuilder UseSquidexRobotsTxt(this IApplicationBuilder app)
+    {
+        app.Map("/robots.txt", builder => builder.UseMiddleware<RobotsTxtMiddleware>());
         return app;
     }
 
@@ -117,19 +119,25 @@ public static class WebExtensions
         return app;
     }
 
-    public static IApplicationBuilder UseSquidexRobotsTxt(this IApplicationBuilder app)
-    {
-        app.Map("/robots.txt", builder => builder.UseMiddleware<RobotsTxtMiddleware>());
-
-        return app;
-    }
-
-    public static void UseSquidexCors(this IApplicationBuilder app)
+    public static IApplicationBuilder UseSquidexCors(this IApplicationBuilder app)
     {
         app.UseCors(builder => builder
             .SetIsOriginAllowed(x => true)
             .AllowCredentials()
             .AllowAnyMethod()
             .AllowAnyHeader());
+
+        return app;
+    }
+
+    public static IApplicationBuilder UseSquidexCompression(this IApplicationBuilder app)
+    {
+        var compressionOptions = app.ApplicationServices.GetRequiredService<IOptions<CompressionOptions>>().Value;
+        if (compressionOptions.Enabled)
+        {
+            app.UseResponseCompression();
+        }
+
+        return app;
     }
 }
