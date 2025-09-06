e3b22f92d38b6e024aeaafcbf19674f22c9238b6
NoahHahm <dbgtdbz2@naver.com>
2025-09-06 23:17:03 +0900

    Style missing header

--- backend/extensions/Squidex.Extensions/Actions/RuleEventPlugin.cs
+++ backend/extensions/Squidex.Extensions/Actions/RuleEventPlugin.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Squidex.Domain.Apps.Entities.Backup;
 using Squidex.Infrastructure.Plugins;
